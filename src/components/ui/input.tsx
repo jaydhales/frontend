b65863eed67c0d6cbf039a7e79fdaef14b18f950
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-04-25 12:44:06 -0700

    feat: Added input variants

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,21 +1,29 @@
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
+import { VariantProps, cva } from "class-variance-authority";
 
-export type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
-
+const InputVariants = cva(
+  "flex h-10 w-40  rounded-md  px-3 py-2 text-sm ring-offset-background" +
+    "file:border-0 file:bg-transparent file:text-sm file:font-medium" +
+    "placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2" +
+    "focus:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+  {
+    variants: {
+      background: { transparent: "bg-transparent" },
+      textSize: { md: "", xl: "text-[40px]" },
+    },
+    defaultVariants: { background: "transparent" },
+  },
+);
+export type InputProps = React.InputHTMLAttributes<HTMLInputElement> &
+  VariantProps<typeof InputVariants>;
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
-  ({ className, type, ...props }, ref) => {
+  ({ className, type, background, textSize, ...props }, ref) => {
     return (
       <input
         type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background" +
-            "file:border-0 file:bg-transparent file:text-sm file:font-medium" +
-            "placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2" +
-            "focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className,
-        )}
+        className={cn(InputVariants({ background, textSize, className }))}
         ref={ref}
         {...props}
       />
