1a6b886292c0eeae65508b2c9292a7c3626714cb
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-04-25 10:07:08 -0700

    changed logo src

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -22,7 +22,7 @@ export function Header() {
     <div className=" grid grid-cols-5 items-center justify-between px-[14px] py-[24px] text-white">
       <div className="flex items-center gap-x-2">
         <Image
-          src={sir_logo}
+          src={"sir-logo.svg"}
           alt="Sir-Trading Logo"
           className="h-[60px] w-auto"
         />
