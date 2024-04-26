ddb37998db91fd340cee31a5c44c0131e24a5ea8
TarpCityBitch <travistarpley0@gmail.com>
2024-04-25 22:54:11 -0700

    refactor: lev tier numbers

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -4,7 +4,7 @@ export const RPC_URL = "https://docker-anvil-production-aa2c.up.railway.app/";
 export const WETH_ADDRESS = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
 export const LeverageTiers = {
   [LeverageTier.one]: 10000n,
-  [LeverageTier.two]: 10000n,
-  [LeverageTier.three]: 10000n,
-  [LeverageTier.four]: 10000n,
+  [LeverageTier.two]: 20000n,
+  [LeverageTier.three]: 30000n,
+  [LeverageTier.four]: 40000n,
 };
