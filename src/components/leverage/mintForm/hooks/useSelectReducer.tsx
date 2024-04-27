de36f2fc3b040cdea9b7a01df2b8169a2fb4ef53
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-04-26 18:17:49 -0700

    refactor:(useSelectReducer): Trying a new way to sort dropdowns.

--- src/components/leverage/mintForm/hooks/useSelectReducer.tsx
+++ src/components/leverage/mintForm/hooks/useSelectReducer.tsx
@@ -57,104 +57,134 @@ enum DropsList {
 //be able to set parent selector to null -> reset everything
 //     ====
 export function useSelectReducer({ formData }: Props) {
-  const [matchingPools, setMatchingPools] = useState<TPool[]>(mockPools);
-  const [parent, setParent] = useState<DropsList | undefined>();
-  useEffect(() => {
-    // TODO
-    // CHECK IF PARENT IS ALREADY SET
-    if (parent) return;
-    if (formData.leverageTier) {
-      const newPools = mockPools.filter(
-        (p) =>
-          LeverageTiers[p.leverageTier] ===
-          LeverageTiers[parseInt(formData.leverageTier) as LeverageTier],
-      );
-      setParent(DropsList.leverageTier);
-      setMatchingPools(newPools);
-    }
-    if (formData.versus) {
-      const newPools = mockPools.filter(
-        (p) => p.collateralToken === formData.versus,
-      );
+  // const [matchingPools, setMatchingPools] = useState<TPool[]>(mockPools);
+  // const [firstSelected, setfirstSelected] = useState<DropsList | undefined>();
+  // const [secondSelected, setSecondSelected] = useState<DropsList | undefined>();
+  // useEffect(() => {
+  //   // TODO
+  //   // CHECK IF firstSelected IS ALREADY SET
+  //   if (firstSelected) return;
+  //   if (formData.leverageTier) {
+  //     const newPools = mockPools.filter(
+  //       (p) =>
+  //         LeverageTiers[p.leverageTier] ===
+  //         LeverageTiers[parseInt(formData.leverageTier) as LeverageTier],
+  //     );
+  //     setfirstSelected(DropsList.leverageTier);
+  //     setMatchingPools(newPools);
+  //   }
+  //   if (formData.versus) {
+  //     const newPools = mockPools.filter(
+  //       (p) => p.collateralToken === formData.versus,
+  //     );
 
-      setParent(DropsList.versus);
-      setMatchingPools(newPools);
-    }
-    if (formData.long) {
-      const newPools = mockPools.filter((p) => {
-        return p.debtToken === formData.long;
-      });
+  //     setfirstSelected(DropsList.versus);
+  //     setMatchingPools(newPools);
+  //   }
+  //   if (formData.long) {
+  //     const newPools = mockPools.filter((p) => {
+  //       return p.debtToken === formData.long;
+  //     });
 
-      setParent(DropsList.long);
-      setMatchingPools(newPools);
-    }
-  }, [formData.leverageTier, formData.versus, formData.long]);
+  //     setfirstSelected(DropsList.long);
+  //     setMatchingPools(newPools);
+  //   }
+  // }, [formData.leverageTier, formData.versus, formData.long]);
 
-  // remove parent if they are now undefined/null
-  useEffect(() => {
-    if (!parent) return;
-    if (parent === DropsList.leverageTier) {
-      if (!formData.leverageTier) {
-        setParent(undefined);
-        setMatchingPools(mockPools);
-      } else {
-        const newPools = mockPools.filter(
-          (p) =>
-            LeverageTiers[p.leverageTier] ===
-            LeverageTiers[parseInt(formData.leverageTier) as LeverageTier],
-        );
-        setMatchingPools(newPools);
-      }
-    }
-    if (parent === DropsList.versus && !formData.versus) {
-      setParent(undefined);
-      setMatchingPools(mockPools);
-    }
-    if (parent === DropsList.long && !formData.long) {
-      setParent(undefined);
-      setMatchingPools(mockPools);
-    }
-  }, [formData.leverageTier, formData.versus, formData.long, parent]);
+  // // remove firstSelected if they are now undefined/null
+  // useEffect(() => {
+  //   if (!firstSelected) return;
+  //   if (firstSelected === DropsList.leverageTier) {
+  //     if (!formData.leverageTier) {
+  //       setfirstSelected(undefined);
+  //       setMatchingPools(mockPools);
+  //     } else {
+  //       const newPools = mockPools.filter(
+  //         (p) =>
+  //           LeverageTiers[p.leverageTier] ===
+  //           LeverageTiers[parseInt(formData.leverageTier) as LeverageTier],
+  //       );
+  //       setMatchingPools(newPools);
+  //       setSecondSelected(undefined);
+  //     }
+  //   }
+  //   if (firstSelected === DropsList.versus && !formData.versus) {
+  //     setfirstSelected(undefined);
+  //     setMatchingPools(mockPools);
+  //   }
+  //   if (firstSelected === DropsList.long && !formData.long) {
+  //     setfirstSelected(undefined);
+  //     setMatchingPools(mockPools);
+  //   }
+  // }, [formData.leverageTier, formData.versus, formData.long, firstSelected]);
 
-  // Find selections for all dropdowns
+  // // Find selections for all dropdowns
+  // const { versus, leverageTiers, long } = useMemo(() => {
+  //   // TODO
+  //   // also filter for second option selected
+  //   // check if select is firstSelected select
+  //   let leverageTiers: LeverageTier[] = [];
+  //   let versus: string[] = [];
+  //   let long: string[] = [];
+
+  //   if (firstSelected !== DropsList.leverageTier) {
+  //     const leverageArr = matchingPools.map((e) => e.leverageTier);
+  //     // Get Unique leverage tiers
+  //     leverageTiers = [...new Set(leverageArr)];
+  //   } else {
+  //     const leverageArr = mockPools.map((e) => e.leverageTier);
+  //     // Get Unique leverage tiers
+  //     leverageTiers = [...new Set(leverageArr)];
+  //   }
+
+  //   if (firstSelected !== DropsList.versus) {
+  //     versus = matchingPools.map((e) => {
+  //       return e.collateralToken;
+  //     });
+  //   } else {
+  //     versus = mockPools.map((e) => e.collateralToken);
+  //   }
+
+  //   if (firstSelected !== DropsList.long) {
+  //     long = matchingPools.map((e) => {
+  //       return e.debtToken;
+  //     });
+  //   } else {
+  //     long = mockPools.map((e) => {
+  //       return e.debtToken;
+  //     });
+  //   }
+
+  //   return { versus, leverageTiers, long };
+  // }, [matchingPools, parent]);
   const { versus, leverageTiers, long } = useMemo(() => {
-    // TODO
-    // also filter for second option selected
-    // check if select is parent select
-    let leverageTiers: LeverageTier[] = [];
-    let versus: string[] = [];
-    let long: string[] = [];
-
-    if (parent !== DropsList.leverageTier) {
-      const leverageArr = matchingPools.map((e) => e.leverageTier);
-      // Get Unique leverage tiers
-      leverageTiers = [...new Set(leverageArr)];
-    } else {
-      const leverageArr = mockPools.map((e) => e.leverageTier);
-      // Get Unique leverage tiers
-      leverageTiers = [...new Set(leverageArr)];
-    }
-
-    if (parent !== DropsList.versus) {
-      versus = matchingPools.map((e) => {
-        return e.collateralToken;
-      });
-    } else {
-      versus = mockPools.map((e) => e.collateralToken);
-    }
-
-    if (parent !== DropsList.long) {
-      long = matchingPools.map((e) => {
-        return e.debtToken;
-      });
-    } else {
-      long = mockPools.map((e) => {
-        return e.debtToken;
-      });
-    }
-
-    return { versus, leverageTiers, long };
-  }, [matchingPools, parent]);
-
+    const matchingPools = mockPools.filter((p) => {
+      if (formData.leverageTier) {
+        if (
+          LeverageTiers[p.leverageTier] !==
+          LeverageTiers[parseInt(formData.leverageTier) as LeverageTier]
+        ) {
+          return false;
+        }
+      }
+      if (formData.versus) {
+        if (formData.versus === p.collateralToken) {
+          return false;
+        }
+      }
+      if (formData.long) {
+        if (formData.long === p.debtToken) {
+          return false;
+        }
+      }
+      return true;
+    });
+    const versus = [...new Set(matchingPools.map((p) => p.collateralToken))];
+    const long = [...new Set(matchingPools.map((p) => p.debtToken))];
+    const leverageTiers = [
+      ...new Set(matchingPools.map((p) => p.leverageTier)),
+    ];
+    return { leverageTiers, long, versus };
+  }, [mockPools, formData.leverageTier, formData.long, formData.versus]);
   return { versus, leverageTiers, long };
 }
