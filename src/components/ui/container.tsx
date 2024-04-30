4e0af54f879d63d274ec7e3d348e7f6b93ab16b4
TarpCityBitch <travistarpley0@gmail.com>
2024-04-29 17:08:35 -0700

    refactor(container): Make container reusable padding

--- src/components/ui/container.tsx
+++ src/components/ui/container.tsx
@@ -9,7 +9,11 @@ export type ContainerProps = React.HtmlHTMLAttributes<HTMLDivElement> & {
 const Container = React.forwardRef<HTMLDivElement, ContainerProps>(
   ({ className, children, ...props }, ref) => {
     return (
-      <div className={cn("mx-auto max-w-7xl", className)} ref={ref} {...props}>
+      <div
+        className={cn("mx-auto max-w-7xl px-4", className)}
+        ref={ref}
+        {...props}
+      >
         {children}
       </div>
     );
