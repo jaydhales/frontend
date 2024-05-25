f7da4ba4aa6a6f8a205e80b7c1b003a19ef208a5
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-05-25 15:23:17 -0700

    feat: new rpc url

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -1,6 +1,6 @@
 import { LeverageTier } from "@/lib/types";
 
-export const RPC_URL = "https://docker-anvil-production-aa2c.up.railway.app/";
+export const RPC_URL = "https://docker-anvil-production-8d23.up.railway.app";
 export const WETH_ADDRESS = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
 export const LeverageTiers = {
   [LeverageTier.one]: -1,
