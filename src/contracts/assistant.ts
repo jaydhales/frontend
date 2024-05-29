3c514af81549acb05a5adc00a556726013be4146
TarpCityBitch <travistarpley0@gmail.com>
2024-05-29 15:20:45 -0700

    feat: Address change

--- src/contracts/assistant.ts
+++ src/contracts/assistant.ts
@@ -1,5 +1,5 @@
 export const Assistant = {
-  address: "0x2f321ed425c82E74925488139e1556f9B76a2551" as `0x${string}`,
+  address: "0x41219a0a9C0b86ED81933c788a6B63Dfef8f17eE" as `0x${string}`,
   abi: [
     {
       inputs: [
