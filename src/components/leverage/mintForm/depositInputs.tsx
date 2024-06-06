e881ee43b34b36c6439ee6463e1d7d7f6064a588
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-06-06 10:48:07 -0700

    feat: Set deposit input 25% 50% max

--- src/components/leverage/mintForm/depositInputs.tsx
+++ src/components/leverage/mintForm/depositInputs.tsx
@@ -85,7 +85,41 @@ export default function DepositInputs({
         <h2 className="pt-1 text-right text-sm text-[#B6B6C9]">
           Balance: {parseFloat(parseFloat(balance ?? "0").toFixed(6))}
         </h2>
-        <h2 className="text-right text-[#26DEC8]">25% 50% Max</h2>
+        <h2 className="pt-1 text-right text-sm text-[#26DEC8]">
+          <span
+            onClick={() =>
+              form.setValue(
+                "deposit",
+                (parseFloat(balance ?? "0") / 4).toString(),
+              )
+            }
+            aria-label="25% Balance"
+            role="button"
+          >
+            25%
+          </span>{" "}
+          <span
+            onClick={() =>
+              form.setValue(
+                "deposit",
+                (parseFloat(balance ?? "0") / 2).toString(),
+              )
+            }
+            aria-label="50% Balance"
+            role="button"
+          >
+            50%
+          </span>{" "}
+          <span
+            onClick={() =>
+              form.setValue("deposit", parseFloat(balance ?? "0").toString())
+            }
+            role="button"
+            aria-label="Max Balance"
+          >
+            Max
+          </span>
+        </h2>
       </div>
     </div>
   );
