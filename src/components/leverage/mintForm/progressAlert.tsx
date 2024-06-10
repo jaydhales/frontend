7db8f3960ce461059684f1d50b5f763c5046eaf7
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-06-10 10:16:52 -0700

    refactor(progress-alert): useState -> useMemo

--- src/components/leverage/mintForm/progressAlert.tsx
+++ src/components/leverage/mintForm/progressAlert.tsx
@@ -1,27 +1,42 @@
 import { AlertDialog, AlertDialogContent } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 interface Props {
   waitForSign: boolean;
   isTxPending: boolean;
+  isTxSuccess: boolean;
 }
-export default function ProgressAlert({ waitForSign, isTxPending }: Props) {
+export default function ProgressAlert({
+  waitForSign,
+  isTxPending,
+  isTxSuccess,
+}: Props) {
   const [o, setOpen] = useState(false);
-  const [message, setMessage] = useState("");
   useEffect(() => {
-    if (waitForSign) {
-      setOpen(true);
-      setMessage("Please Sign Transaction.");
-    }
     if (isTxPending) {
       setOpen(true);
-      setMessage("Waiting for block.");
     }
-    if (!waitForSign && !isTxPending) {
-      setOpen(false);
+    if (waitForSign) {
+      setOpen(true);
     }
-  }, [waitForSign, isTxPending]);
+    if (isTxSuccess) {
+      setOpen(true);
+    }
+  }, [isTxPending, isTxSuccess, waitForSign]);
+
+  const data = useMemo(() => {
+    if (isTxSuccess) {
+      return { message: "Success!", success: true };
+    }
+    if (waitForSign) {
+      return { message: "Please Sign Transaction." };
+    }
+    if (isTxPending) {
+      return { message: "Pending..." };
+    }
+    return {};
+  }, [waitForSign, isTxPending, isTxSuccess]);
   return (
     <AlertDialog open={o} onOpenChange={setOpen}>
       <AlertDialogContent
@@ -30,8 +45,10 @@ export default function ProgressAlert({ waitForSign, isTxPending }: Props) {
         closeColor={"black"}
         className=" overflow-hidden bg-transparent"
       >
-        <div className="space-y-3 rounded-md bg-secondary p-8 text-white">
-          <h2 className="pb-1 text-center font-lora text-xl">{message}</h2>
+        <div
+          className={`space-y-3 rounded-md border-4 border-transparent transition-all duration-500 ${data.success ? "border-accent" : ""} bg-secondary p-8 text-white`}
+        >
+          <h2 className="pb-1 text-center font-lora text-xl">{data.message}</h2>
           <h3 className="text-gray">Lorem ipsum</h3>
           <Button
             role="button"
