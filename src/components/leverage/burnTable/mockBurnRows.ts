4f9e9b988b4beadf79b1ada431cd26c6f69880f7
Travis <travistarpley0@gmail.com>
2024-06-25 12:42:39 -0700

    feat: type fix

--- src/components/leverage/burnTable/mockBurnRows.ts
+++ src/components/leverage/burnTable/mockBurnRows.ts
@@ -1,36 +1,4 @@
 import { LeverageTier, type TBurnRow } from "@/lib/types";
 
 export const burnRows: TBurnRow[] = [
-  {
-    tokenId: "1",
-    amount: "200",
-    tokenLong: "0x1",
-    tokenVersus: "0x2",
-    leverageRatio: LeverageTier.one,
-    balance: "200",
-  },
-  {
-    tokenId: "2",
-    amount: "200",
-    tokenLong: "0x1",
-    tokenVersus: "0x2",
-    leverageRatio: LeverageTier.one,
-    balance: "200",
-  },
-  {
-    tokenId: "3",
-    amount: "200",
-    tokenLong: "0x1",
-    tokenVersus: "0x2",
-    leverageRatio: LeverageTier.one,
-    balance: "200",
-  },
-  {
-    tokenId: "4",
-    amount: "200",
-    tokenLong: "0x1",
-    tokenVersus: "0x2",
-    leverageRatio: LeverageTier.one,
-    balance: "200",
-  },
 ];
