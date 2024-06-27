b540d53ea3716c22cf9739e740a33dfa0a7206cb
Tarp <travistarpley0@gmail.com>
2024-06-27 14:55:32 -0700

    fix: rpc fix

--- src/components/providers/evmProvider.tsx
+++ src/components/providers/evmProvider.tsx
@@ -1,18 +1,17 @@
 "use client";
 import "@rainbow-me/rainbowkit/styles.css";
 import {
-  cookieStorage,
-  cookieToInitialState,
-  createStorage,
-  WagmiProvider,
+    cookieStorage,
+    cookieToInitialState,
+    createStorage,
+    WagmiProvider,
 } from "wagmi";
 import { mainnet } from "wagmi/chains";
 import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
-import { RPC_URL } from "@/data/constants";
 const main = {
-  ...mainnet,
-  rpcUrls: { default: { http: [RPC_URL] } },
-  id: 1,
+    ...mainnet,
+    rpcUrls: { default: { http: ["http://140.82.62.189:8545/"] } },
+    id: 1,
 };
 
 // const config = getDefaultConfig({
@@ -23,28 +22,29 @@ const main = {
 // });
 
 export const wagmiConfig = getDefaultConfig({
-  appName: "SIR",
-  projectId:
-    process.env.NEXT_PUBLIC_PROJECT_ID ?? "934acc697f01fec33b75c19d9bb2e3c7",
-  chains: [main],
-  ssr: true,
-  storage: createStorage({
-    storage: cookieStorage,
-  }),
+    appName: "SIR",
+    projectId:
+        process.env.NEXT_PUBLIC_PROJECT_ID ??
+        "934acc697f01fec33b75c19d9bb2e3c7",
+    chains: [main],
+    ssr: true,
+    storage: createStorage({
+        storage: cookieStorage,
+    }),
 });
 function EvmProvider({
-  children,
-  cookie,
+    children,
+    cookie,
 }: {
-  children: React.ReactNode;
-  cookie: string | null;
+    children: React.ReactNode;
+    cookie: string | null;
 }) {
-  const initialState = cookieToInitialState(wagmiConfig, cookie);
-  return (
-    <WagmiProvider config={wagmiConfig} initialState={initialState}>
-      <RainbowKitProvider>{children}</RainbowKitProvider>
-    </WagmiProvider>
-  );
+    const initialState = cookieToInitialState(wagmiConfig, cookie);
+    return (
+        <WagmiProvider config={wagmiConfig} initialState={initialState}>
+            <RainbowKitProvider>{children}</RainbowKitProvider>
+        </WagmiProvider>
+    );
 }
 
 export default EvmProvider;
