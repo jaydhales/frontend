657a4c4e9432c7cb4266189bfd4948db4edf2d09
Tarp <travistarpley0@gmail.com>
2024-06-27 17:18:22 -0700

    fix: formatting fix

--- prettier.config.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
-const config = {
-    tabWidth: 4,
-    plugins: ["prettier-plugin-tailwindcss"],
-};
-
-export default config;
--- src/components/leverage/mintForm/depositInputs.tsx
+++ src/components/leverage/mintForm/depositInputs.tsx
@@ -8,130 +8,130 @@ import { getLogoAsset } from "@/lib/utils";
 import { Switch } from "@/components/ui/switch";
 import { WETH_ADDRESS } from "@/data/constants";
 interface Props {
-        form: TMintForm;
-        depositAsset: string | undefined;
-        balance?: string;
-        useEth: boolean;
-        setUseEth: (b: boolean) => void;
+    form: TMintForm;
+    depositAsset: string | undefined;
+    balance?: string;
+    useEth: boolean;
+    setUseEth: (b: boolean) => void;
 }
 export default function DepositInputs({
-        form,
-        depositAsset,
-        balance,
-        useEth,
-        setUseEth,
+    form,
+    depositAsset,
+    balance,
+    useEth,
+    setUseEth,
 }: Props) {
-        console.log({ useEth });
-        return (
-                <div className="flex justify-between rounded-md bg-primary p-3">
-                        <div>
-                                <FormField
-                                        control={form.control}
-                                        name="deposit"
-                                        render={({ field }) => (
-                                                <FormItem>
-                                                        <FormControl>
-                                                                <Input
-                                                                        type="text"
-                                                                        inputMode="decimal"
-                                                                        autoComplete="off"
-                                                                        pattern="^[0-9]*[.,]?[0-9]*$"
-                                                                        background="primary"
-                                                                        placeholder="0"
-                                                                        minLength={1}
-                                                                        textSize="xl"
-                                                                        step="any"
-                                                                        {...field}
-                                                                        onChange={(e) => {
-                                                                                const pattern = /^[0-9]*[.,]?[0-9]*$/;
-                                                                                if (pattern.test(e.target.value))
-                                                                                        return field.onChange(
-                                                                                                e.target.value,
-                                                                                        );
-                                                                        }}
-                                                                ></Input>
-                                                        </FormControl>
-                                                </FormItem>
-                                        )}
-                                />
+    console.log({ useEth });
+    return (
+        <div className="flex justify-between rounded-md bg-primary p-3">
+            <div>
+                <FormField
+                    control={form.control}
+                    name="deposit"
+                    render={({ field }) => (
+                        <FormItem>
+                            <FormControl>
+                                <Input
+                                    type="text"
+                                    inputMode="decimal"
+                                    autoComplete="off"
+                                    pattern="^[0-9]*[.,]?[0-9]*$"
+                                    background="primary"
+                                    placeholder="0"
+                                    minLength={1}
+                                    textSize="xl"
+                                    step="any"
+                                    {...field}
+                                    onChange={(e) => {
+                                        const pattern = /^[0-9]*[.,]?[0-9]*$/;
+                                        if (pattern.test(e.target.value))
+                                            return field.onChange(
+                                                e.target.value,
+                                            );
+                                    }}
+                                ></Input>
+                            </FormControl>
+                        </FormItem>
+                    )}
+                />
 
-                                <div className="">
-                                        {depositAsset?.split(",")[0] === WETH_ADDRESS && (
-                                                <div className="flex items-center gap-x-2 pt-1">
-                                                        <h3 className="text-[12px]">Use ETH</h3>
-                                                        <Switch
-                                                                checked={useEth}
-                                                                onCheckedChange={() => {
-                                                                        setUseEth(!useEth);
-                                                                }}
-                                                                aria-readonly
-                                                        />
-                                                </div>
-                                        )}
-                                </div>
-                                {/* <h2 className="pt-1 text-sm text-[#B6B6C9]">$22.55</h2> */}
+                <div className="">
+                    {depositAsset?.split(",")[0] === WETH_ADDRESS && (
+                        <div className="flex items-center gap-x-2 pt-1">
+                            <h3 className="text-[12px]">Use ETH</h3>
+                            <Switch
+                                checked={useEth}
+                                onCheckedChange={() => {
+                                    setUseEth(!useEth);
+                                }}
+                                aria-readonly
+                            />
                         </div>
-
-                        <div className="flex flex-col items-end">
-                                <h2 className="pb-2 text-sm">Deposit Asset:</h2>
-                                <div
-                                        className={`flex w-[104px] items-center justify-center gap-x-2 rounded-md bg-secondary py-1 ${!depositAsset ? "opacity-70" : ""}`}
-                                >
-                                        {!depositAsset && <div className="h-[25px] w-[25px]"></div>}
-                                        <AssetInfo depositAsset={depositAsset} useEth={useEth} />
-                                </div>
-                                <h2 className="pt-1 text-right text-sm text-[#B6B6C9]">
-                                        Balance: {parseFloat(parseFloat(balance ?? "0").toFixed(4))}
-                                </h2>
-                                <div className="flex justify-end"></div>
-                                <BalancePercent
-                                        balance={balance}
-                                        setValue={(s: string) => {
-                                                form.setValue("deposit", s);
-                                        }}
-                                />
-                        </div>
+                    )}
                 </div>
-        );
+                {/* <h2 className="pt-1 text-sm text-[#B6B6C9]">$22.55</h2> */}
+            </div>
+
+            <div className="flex flex-col items-end">
+                <h2 className="pb-2 text-sm">Deposit Asset:</h2>
+                <div
+                    className={`flex w-[104px] items-center justify-center gap-x-2 rounded-md bg-secondary py-1 ${!depositAsset ? "opacity-70" : ""}`}
+                >
+                    {!depositAsset && <div className="h-[25px] w-[25px]"></div>}
+                    <AssetInfo depositAsset={depositAsset} useEth={useEth} />
+                </div>
+                <h2 className="pt-1 text-right text-sm text-[#B6B6C9]">
+                    Balance: {parseFloat(parseFloat(balance ?? "0").toFixed(4))}
+                </h2>
+                <div className="flex justify-end"></div>
+                <BalancePercent
+                    balance={balance}
+                    setValue={(s: string) => {
+                        form.setValue("deposit", s);
+                    }}
+                />
+            </div>
+        </div>
+    );
 }
 
 function AssetInfo({
-        useEth,
-        depositAsset,
+    useEth,
+    depositAsset,
 }: {
-        useEth: boolean;
-        depositAsset: string | undefined;
+    useEth: boolean;
+    depositAsset: string | undefined;
 }) {
-        if (useEth) {
-                return (
-                        <>
-                                {depositAsset && (
-                                        <Image
-                                                src={
-                                                        "https://raw.githubusercontent.com/fusionxx23/assets/master/blockchains/ethereum/info/logo.png"
-                                                }
-                                                alt={"ETH"}
-                                                width={25}
-                                                height={25}
-                                        />
-                                )}
-                                <span>{"ETH"}</span>
-                        </>
-                );
-        }
+    if (useEth) {
         return (
-                <>
-                        {depositAsset && (
-                                <Image
-                                        src={getLogoAsset(
-                                                depositAsset?.split(",")[0] as TAddressString,
-                                        )}
-                                        alt={depositAsset.split(",")[1] ?? ""}
-                                        width={25}
-                                        height={25}
-                                />
-                        )}
-                        <span>{depositAsset?.split(",")[1]}</span>
-                </>
+            <>
+                {depositAsset && (
+                    <Image
+                        src={
+                            "https://raw.githubusercontent.com/fusionxx23/assets/master/blockchains/ethereum/info/logo.png"
+                        }
+                        alt={"ETH"}
+                        width={25}
+                        height={25}
+                    />
+                )}
+                <span>{"ETH"}</span>
+            </>
         );
+    }
+    return (
+        <>
+            {depositAsset && (
+                <Image
+                    src={getLogoAsset(
+                        depositAsset?.split(",")[0] as TAddressString,
+                    )}
+                    alt={depositAsset.split(",")[1] ?? ""}
+                    width={25}
+                    height={25}
+                />
+            )}
+            <span>{depositAsset?.split(",")[1]}</span>
+        </>
+    );
 }
