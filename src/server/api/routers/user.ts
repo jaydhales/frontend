a4944aea76476204b77c6358e32feb68da02ed71
Tarp <travistarpley0@gmail.com>
2024-07-01 11:06:31 -0700

    refactor: moved UserQuery type to libs/type

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { getBalance, multicall, readContract } from "@/lib/viemClient";
 import { erc20Abi } from "viem";
-import { type TAddressString } from "@/lib/types";
+import { UserQuery, type TAddressString } from "@/lib/types";
 import { executeGetUserVaultsQuery } from "@/server/queries/vaults";
 
 export const userRouter = createTRPCRouter({
@@ -98,18 +98,6 @@ export const userRouter = createTRPCRouter({
     }),
 });
 //todo use ZOD
-type UserQuery = {
-  userPositions: {
-    User: string;
-    leverageTier: string;
-    balance: bigint;
-    APE: string;
-    collateralToken: string;
-    debtToken: string;
-    collateralSymbol: string;
-    debtSymbol: string;
-  }[];
-};
 
 // create: publicProcedure
 //   .input(z.object({ name: z.string().min(1) }))
