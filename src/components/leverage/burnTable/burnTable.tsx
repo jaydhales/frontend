e2fae4965f537596ba6d6046636667d5c6c20366
Tarp <travistarpley0@gmail.com>
2024-07-02 14:20:08 -0700

    fix: Fixed table html error

--- src/components/leverage/burnTable/burnTable.tsx
+++ src/components/leverage/burnTable/burnTable.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 "use client";
 import React, { useMemo, useState } from "react";
-import { burnRows } from "./mockBurnRows";
+// import { burnRows } from "./mockBurnRows";
 import BurnTableHeaders from "./burnTableHeader";
 import BurnTableRow from "./burnTableRow";
 import SelectedRow from "./selected-row";
@@ -11,12 +11,10 @@ export default function BurnTable() {
   const [selectedRow, setSelectedRow] = useState<string | undefined>();
 
   const { data } = useBurnTableProvider();
-
   const selectedRowParams = useMemo(() => {
     return data?.userPositions.find((r) => r.APE === selectedRow);
   }, [data?.userPositions, selectedRow]);
-  console.log(burnRows);
-  console.log({ selectedRowParams });
+
   return (
     <div className="relative">
       {selectedRow && (
@@ -31,19 +29,21 @@ export default function BurnTable() {
       {!selectedRow && (
         <table className="flex flex-col gap-y-4">
           <caption className="hidden">Burn Tokens</caption>
-          <BurnTableHeaders />
-          {data?.userPositions.map((r) => (
-            <BurnTableRow
-              setSelectedRow={setSelectedRow}
-              apeAddress={r.APE}
-              colSymbol={r.collateralSymbol}
-              leverageTier={r.leverageTier}
-              debtSymbol={r.debtSymbol}
-              debtToken={r.debtToken}
-              colToken={r.collateralToken}
-              key={r.APE}
-            ></BurnTableRow>
-          ))}
+          <tbody>
+            <BurnTableHeaders />
+            {data?.userPositions.map((r) => (
+              <BurnTableRow
+                setSelectedRow={setSelectedRow}
+                apeAddress={r.APE}
+                colSymbol={r.collateralSymbol}
+                leverageTier={r.leverageTier}
+                debtSymbol={r.debtSymbol}
+                debtToken={r.debtToken}
+                colToken={r.collateralToken}
+                key={r.APE}
+              ></BurnTableRow>
+            ))}
+          </tbody>
         </table>
       )}
     </div>
