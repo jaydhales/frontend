40dd27243130628e2a712c5a05a8f73f59125e1d
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-07-05 15:35:33 -0700

    feat: added liquidity page

--- src/app/liquidity/page.tsx
+++ src/app/liquidity/page.tsx
@@ -5,6 +5,7 @@ import { api } from "@/trpc/server";
 
 export default async function Home() {
   const vaultQuery = await api.vault.getVaults();
+
   return (
     <main className="flex flex-col items-center justify-center text-white">
       <LeveragePage>
