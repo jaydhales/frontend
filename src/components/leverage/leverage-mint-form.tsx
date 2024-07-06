ba1504596d32bda861405efa7ddf6050c191c648
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-07-05 17:11:36 -0700

    feat: Moved leverage-mint-form unique logic to seperate file

---
+++ src/components/leverage/leverage-mint-form.tsx
@@ -0,0 +1,65 @@
+"use client";
+import { findVault, formatDataInput } from "@/lib/utils";
+import MintForm from "../shared/mintForm/mint-form";
+import { useMintApe } from "../shared/hooks/useMintApe";
+import { useFormContext } from "react-hook-form";
+import { AssistantContract } from "@/contracts/assistant";
+import type { SimulateContractReturnType } from "viem";
+import { parseUnits } from "viem";
+import type { TMintFormFields, TVaults } from "@/lib/types";
+import { useMemo } from "react";
+import { z } from "zod";
+import { useAccount } from "wagmi";
+import { api } from "@/trpc/react";
+import { useApprove } from "../shared/mintForm/hooks/useApprove";
+type SimulateReq = SimulateContractReturnType["request"] | undefined;
+export function LeverageMintForm({ vaultsQuery }: { vaultsQuery: TVaults }) {
+  const form = useFormContext<TMintFormFields>();
+  const formData = form.watch();
+  /** ##MINT APE## */
+  const { address } = useAccount();
+  const { data: userBalance } = api.user.getBalance.useQuery(
+    {
+      userAddress: address,
+      tokenAddress: formatDataInput(formData.long),
+      spender: AssistantContract.address,
+    },
+    { enabled: Boolean(address) && Boolean(formData.long) },
+  );
+
+  const safeLeverageTier = z.coerce.number().safeParse(formData.leverageTier);
+  const leverageTier = safeLeverageTier.success ? safeLeverageTier.data : -1;
+  const safeDeposit = useMemo(() => {
+    return z.coerce.number().safeParse(formData.deposit);
+  }, [formData.deposit]);
+  const {
+    Mint,
+    MintWithEth,
+    isFetching: mintFetching,
+  } = useMintApe({
+    vaultId: findVault(vaultsQuery, formData),
+    debtToken: formatDataInput(formData.versus), //value formatted : address,symbol
+    collateralToken: formatDataInput(formData.long), //value formatted : address,symbol
+    leverageTier: leverageTier,
+    amount: safeDeposit.success
+      ? parseUnits(safeDeposit.data.toString() ?? "0", 18)
+      : undefined,
+    tokenAllowance: userBalance?.tokenAllowance?.result,
+  });
+
+  const { approveSimulate } = useApprove({
+    tokenAddr: formatDataInput(formData.long),
+    approveContract: AssistantContract.address,
+  });
+
+  return (
+    <MintForm
+      approveFetching={approveSimulate.isFetching}
+      approveSimulate={approveSimulate.data?.request as SimulateReq}
+      MintWithEth={MintWithEth as SimulateReq}
+      Mint={Mint?.request as SimulateReq}
+      vaultsQuery={vaultsQuery}
+      mintFetching={mintFetching}
+    />
+  );
+}
--- src/components/leverage/leverageTabs.tsx
+++ src/components/leverage/leverageTabs.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../ui/tabs";
 import { Container } from "../ui/container";
 import { Card } from "../ui/card";
-import MintForm from "./mintForm/mintForm";
 import VaultTable from "./vaultTable/vaultTable";
 import BurnTable from "./burnTable/burnTable";
 import MintFormProvider from "../providers/mintFormProvider";
@@ -32,7 +31,6 @@ export default function LeverageTabs({
             <div className="grid w-full gap-x-[16px] gap-y-4 lg:grid-cols-2">
               <MintFormProvider>
                 {form}
-                {/* <MintForm vaultsQuery={vaultsQuery} /> */}
                 <Card>
                   <VaultTable vaultQuery={vaultsQuery} />
                 </Card>
