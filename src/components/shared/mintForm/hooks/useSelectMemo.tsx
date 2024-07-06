3100a4aa86620f1e6f1982a63c2567c8129a8ab5
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-07-05 17:01:20 -0700

    refactor: moved mintForm

--- src/components/shared/mintForm/hooks/useSelectMemo.tsx
+++ src/components/shared/mintForm/hooks/useSelectMemo.tsx
@@ -6,7 +6,7 @@ interface Props {
   vaultsQuery: TVaults;
 }
 /**
- * Narrows down dropdown items when other dropdowns are selected.
+ * Narrows down dropdown items(vaults) when other dropdowns are selected.
  */
 export function useSelectMemo({ formData, vaultsQuery }: Props) {
   const { versus, leverageTiers, long } = useMemo(() => {
