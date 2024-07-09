df2e78d943a124657b1c4f043de5bea3cec7ba86
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-07-09 10:23:04 -0700

    feat: Added liquidity mint form and hook

---
+++ src/components/liquidity/hooks/useMintTea.ts
@@ -0,0 +1,67 @@
+"use client";
+import { useSimulateContract } from "wagmi";
+import { AssistantContract } from "@/contracts/assistant";
+import type { TAddressString } from "@/lib/types";
+import { parseUnits } from "viem";
+import { z } from "zod";
+import { getApeAddress } from "@/lib/utils";
+import { APE_HASH } from "@/data/constants";
+import { VaultContract } from "@/contracts/vault";
+import { useEffect } from "react";
+interface Props {
+  collateralToken: string;
+  debtToken: string;
+  amount: bigint | undefined;
+  tokenAllowance: bigint | undefined;
+  leverageTier: number;
+  vaultId: string | undefined;
+}
+
+export function useMintTea({
+  collateralToken,
+  debtToken,
+  leverageTier,
+  amount,
+  vaultId,
+  tokenAllowance,
+}: Props) {
+  const safeVaultId = z.coerce.number().safeParse(vaultId);
+  const apeAddress = getApeAddress({
+    apeHash: APE_HASH,
+    vaultAddress: VaultContract.address,
+    vaultId: safeVaultId.success ? safeVaultId.data : 0,
+  });
+  console.log(apeAddress);
+  const vault = {
+    debtToken: debtToken as TAddressString,
+    collateralToken: collateralToken as TAddressString,
+    leverageTier,
+  };
+  const {
+    data: Mint,
+    refetch,
+    isFetching,
+  } = useSimulateContract({
+    abi: AssistantContract.abi,
+    address: AssistantContract.address,
+    functionName: "mint",
+    args: [
+      "0x",
+      parseUnits(vaultId?.toString() ?? "0", 0),
+      { ...vault },
+      amount ?? 0n,
+    ],
+  });
+
+  const { data: MintWithEth } = useSimulateContract({
+    ...AssistantContract,
+    functionName: "mintWithETH",
+    args: ["0x", parseUnits(vaultId?.toString() ?? "0", 0), { ...vault }],
+    value: amount ?? 0n,
+  });
+
+  useEffect(() => {
+    refetch().catch((e) => console.log(e));
+  }, [refetch, tokenAllowance]);
+  return { Mint, MintWithEth, isFetching };
+}
