50a25e8e0414e20200e76a51723cee731bd962b9
Thai Nguyen <jooohn.eth@gmail.com>
2024-07-10 16:36:40 -0400

    feature: added tabs

--- src/components/stake/stakeTabs.tsx
+++ src/components/stake/stakeTabs.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+"use client";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Container } from "@/components/ui/container";
+
+// import StakeForm from "./stakeForm/stakeForm";
 
 const StakeTabs = () => {
-  return <div></div>;
+  return (
+    <Tabs defaultValue="stake">
+      <div className="flex justify-center">
+        <TabsList defaultValue={"stake"}>
+          <TabsTrigger value={"stake"}>Stake</TabsTrigger>
+          <TabsTrigger value={"unstake"}>Unstake</TabsTrigger>
+          <TabsTrigger value={"claimFees"}>Claim fees</TabsTrigger>
+        </TabsList>
+      </div>
+      <br />
+      <TabsContent value="stake">
+        <Container>
+          <div>Stake</div>
+        </Container>
+      </TabsContent>
+      <TabsContent value="unstake">
+        <Container>
+          <div>Unstake</div>
+        </Container>
+      </TabsContent>
+      <TabsContent value="claimFees">
+        <Container>
+          <div>Claim Fees</div>
+        </Container>
+      </TabsContent>
+    </Tabs>
+  );
 };
 
 export default StakeTabs;
