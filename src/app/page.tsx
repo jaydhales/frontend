cb4f7ef2319ee556128d03d465b730007c5f12f5
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-07-09 20:43:02 -0700

    refactor: Add mintForm for Tea

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import { LeverageMintForm } from "@/components/leverage/leverage-mint-form";
 import LeveragePage from "@/components/leverage/leveragePage";
 import LeverageTabs from "@/components/leverage/leverageTabs";
+import { MintForm } from "@/components/shared/mintForm/mint-form";
 import { api } from "@/trpc/server";
 
 export default async function Home() {
@@ -9,7 +9,7 @@ export default async function Home() {
     <main className="flex flex-col items-center justify-center text-white">
       <LeveragePage>
         <LeverageTabs
-          form={<LeverageMintForm vaultsQuery={vaultQuery} />}
+          form={<MintForm isApe vaultsQuery={vaultQuery} />}
           vaultsQuery={vaultQuery}
         />
       </LeveragePage>
