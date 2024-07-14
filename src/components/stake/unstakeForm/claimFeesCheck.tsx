94b965aa56041e447e3899c8c9317683d72b611d
Thai Nguyen <jooohn.eth@gmail.com>
2024-07-13 21:21:19 -0400

    feat: added claim fees checkbox

---
+++ src/components/stake/unstakeForm/claimFeesCheck.tsx
@@ -0,0 +1,45 @@
+import {
+  FormField,
+  FormItem,
+  FormControl,
+  FormLabel,
+} from "@/components/ui/form";
+import type { TUnstakeForm } from "@/lib/types";
+
+import { Checkbox } from "@/components/ui/checkbox";
+
+interface Props {
+  form: TUnstakeForm;
+}
+
+const ClaimFeesCheckbox = ({ form }: Props) => {
+  return (
+    <FormField
+      control={form.control}
+      name="claimFees"
+      render={({ field }) => (
+        <FormItem className="mt-[20px]">
+          <FormControl>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-x-4">
+                <Checkbox
+                  {...field}
+                  checked={field.value}
+                  onCheckedChange={field.onChange}
+                ></Checkbox>
+                <FormLabel className="text-lg font-medium">
+                  Claim my fees
+                </FormLabel>
+              </div>
+              <div className="text-[#B6B6C9] text-sm">
+                Amount to claim: {`171.31`} ETH
+              </div>
+            </div>
+          </FormControl>
+        </FormItem>
+      )}
+    ></FormField>
+  );
+};
+
+export default ClaimFeesCheckbox;
