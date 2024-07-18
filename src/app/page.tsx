3e6693d8386dfaeaba9be6b4f706de91b6672a75
Thai Nguyen <jooohn.eth@gmail.com>
2024-07-18 17:02:37 -0400

    fix: minor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import LeveragePage from "@/components/leverage/leveragePage";
 import LeverageTabs from "@/components/leverage/leverageTabs";
 import { MintForm } from "@/components/shared/mintForm/mint-form";
 
-
 import { api } from "@/trpc/server";
 
 export default async function Home() {
@@ -15,7 +14,7 @@ export default async function Home() {
           form={<MintForm isApe vaultsQuery={vaultQuery} />}
           vaultsQuery={vaultQuery}
         />
-
+      </LeveragePage>
     </main>
   );
 }
