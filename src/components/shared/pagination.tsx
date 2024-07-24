221c7375675237f3e24e139982cfc73909bafeb7
Tarp <travistarpley0@gmail.com>
2024-07-23 19:03:37 -0700

    refactor: number -> page

--- src/components/shared/pagination.tsx
+++ src/components/shared/pagination.tsx
@@ -1,24 +1,20 @@
 import { ChevronRight, ChevronLeft } from "lucide-react";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
-export default function Pagination({ max }: { max: number }) {
+export default function Pagination({ max }: { max: page }) {
   if (max < 1) {
     max = 1;
   }
+  console.log(max, "MAX");
   const searchParams = useSearchParams();
-  const pagination = searchParams.get("pagination");
-  let number = pagination ? parseInt(pagination) : 1;
-  if (!isFinite(number)) {
-    // check if pagination is a number
-    number = 1;
+  const pagination = searchParams.get("vault-page");
   }
-
   return (
     <div className="flex justify-end gap-x-3 items-center pt-4">
       <div className="flex gap-x-3 items-center">
-        <Link href="/" scroll={false}>
+        <Link href={"?vault-page=" + (page - 1)} scroll={false}>
           <button
-            disabled={number === 1}
+            disabled={page === 1}
             aria-label="Left"
             className="rounded-full disabled:opacity-50 p-[5px] bg-primary"
           >
@@ -26,17 +22,16 @@ export default function Pagination({ max }: { max: number }) {
           </button>
         </Link>
         <div className="px-3 h-[25px] items-center flex rounded-lg text-[15px] bg-primary">
-          1
+          {page}
         </div>
         <span className="opacity-80">of</span>{" "}
         <div className="px-3 h-[25px] items-center flex text-[15px] rounded-lg bg-primary">
           {max}
         </div>
-        <Link href="/" scroll={false}>
+        <Link href={"?vault-page=" + (page + 1)} scroll={false}>
           <button
             role="link"
             aria-label="Scroll Vaults Right"
-            disabled={number >= max}
             className="rounded-full disabled:opacity-50 p-[5px] bg-primary"
           >
             <ChevronRight size={17} />
