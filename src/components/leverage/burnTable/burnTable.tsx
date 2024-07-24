acee8610e108d2034ac7bac29c5ccd9b2543d24e
Tarp <travistarpley0@gmail.com>
2024-07-23 17:32:34 -0700

    feat: fix merge conflicts

--- src/components/leverage/burnTable/burnTable.tsx
+++ src/components/leverage/burnTable/burnTable.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useMemo, useState } from "react";
-// import { burnRows } from "./mockBurnRows";
 import BurnTableHeaders from "./burnTableHeader";
 import BurnTableRow from "./burnTableRow";
 import SelectedRow from "./selected-row";
@@ -10,10 +9,22 @@ import { useAccount } from "wagmi";
 export default function BurnTable({ isApe }: { isApe: boolean }) {
   const [selectedRow, setSelectedRow] = useState<string | undefined>();
 
-  const { data } = useBurnTableProvider();
-  const selectedRowParams = useMemo(() => {
-    return data?.userPositions.find((r) => r.APE === selectedRow);
-  }, [data?.userPositions, selectedRow]);
+  const { address } = useAccount();
+  const ape = api.user.getApePositions.useQuery(
+    { address },
+    { enabled: isApe },
+  );
+  const tea = api.user.getTeaPositions.useQuery(
+    { address },
+    { enabled: !isApe },
+  );
+
+  const selectedRowParamsApe = useMemo(() => {
+    return ape.data?.userPositions.find((r) => r.vaultId === selectedRow);
+  }, [ape.data?.userPositions, selectedRow]);
+  const selectedRowParamsTea = useMemo(() => {
+    return tea.data?.userPositionTeas.find((r) => r.vaultId === selectedRow);
+  }, [selectedRow, tea.data?.userPositionTeas]);
 
   return (
     <div className="relative">
@@ -45,20 +56,46 @@ export default function BurnTable({ isApe }: { isApe: boolean }) {
       {!selectedRow && (
         <table className="flex flex-col gap-y-4">
           <caption className="hidden">Burn Tokens</caption>
+          <BurnTableHeaders />
           <tbody>
-            <BurnTableHeaders />
-            {data?.userPositions.map((r) => (
-              <BurnTableRow
-                setSelectedRow={setSelectedRow}
-                apeAddress={r.APE}
-                colSymbol={r.collateralSymbol}
-                leverageTier={r.leverageTier}
-                debtSymbol={r.debtSymbol}
-                debtToken={r.debtToken}
-                colToken={r.collateralToken}
-                key={r.APE}
-              ></BurnTableRow>
-            ))}
+            {isApe ? (
+              <>
+                {ape.data?.userPositions.map((r) => (
+                  <BurnTableRow
+                    setSelectedRow={setSelectedRow}
+                    key={r.vaultId}
+                    row={{
+                      id: r.vaultId,
+                      balance: r.balance,
+                      user: r.user,
+                      collateralSymbol: r.collateralSymbol,
+                      debtSymbol: r.debtSymbol,
+                      collateralToken: r.collateralToken,
+                      debtToken: r.debtToken,
+                      leverageTier: r.leverageTier,
+                      vaultId: r.vaultId,
+                    }}
+                    isApe={true}
+                    apeAddress={r.APE}
+                  ></BurnTableRow>
+                ))}
+              </>
+            ) : (
+              <>
+                {tea.data?.userPositionTeas.map((r) => {
+                  return (
+                    <BurnTableRow
+                      row={{
+                        ...r,
+                      }}
+                      key={r.id}
+                      isApe={false}
+                      setSelectedRow={setSelectedRow}
+                    />
+                  );
+                })}
+              </>
+            )}
           </tbody>
         </table>
       )}
