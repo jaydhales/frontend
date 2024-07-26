fe50b92aa0538f8cee77ac932b316fef2c09aeab
Tarp <travistarpley0@gmail.com>
2024-07-26 16:44:46 -0700

    refactor: Added TokenInput Component

--- src/components/create-vault/createVaultForm.tsx
+++ src/components/create-vault/createVaultForm.tsx
@@ -1,13 +1,15 @@
 "use client";
 import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
-import { FormProvider, useForm } from "react-hook-form";
+import { FormProvider, useForm, useFormContext } from "react-hook-form";
 import type { z } from "zod";
-import { isValid } from "zod";
 import { FormControl, FormField, FormItem, FormLabel } from "../ui/form";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { CreateVaultInputValues } from "@/lib/schemas";
+import type { TCreateVaultKeys } from "@/lib/types";
+import { useCreateVault } from "./hooks/useCreateVault";
+import { useWriteContract } from "wagmi";
 export default function CreateVaultForm() {
   const form = useForm<z.infer<typeof CreateVaultInputValues>>({
     resolver: zodResolver(CreateVaultInputValues),
@@ -18,58 +20,38 @@ export default function CreateVaultForm() {
       versusToken: "",
     },
   });
+  const formData = form.watch();
+  const { longToken, versusToken, leverageTier } = formData;
+  const data = useCreateVault({ longToken, versusToken, leverageTier });
+  const { writeContract } = useWriteContract();
+  const onSubmit = () => {
+    if (form.formState.isValid && data?.request) {
+      writeContract(data?.request);
+    }
+  };
+
   return (
     <FormProvider {...form}>
-      <form className="space-y-6">
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
         <div className="grid  gap-y-2">
           <div className="w-full space-y-2">
-            <FormLabel htmlFor="longToken">Long Token</FormLabel>
-            <FormField
-              control={form.control}
-              name="longToken"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      type="text"
-                      placeholder="0x"
-                      autoComplete="off"
-                      className="w-full"
-                      background="primary"
-                      minLength={1}
-                      textSize="md"
-                      step="any"
-                      {...field}
-                    ></Input>
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+            <TokenInput name="longToken" title="Long Token" />
+            <p
+              data-active={formData.longToken.length > 0 ? "true" : "false"}
+              className="text-red-400 text-sm data-[active=false]:hidden"
+            >
+              {form.formState.errors.longToken?.message}
+            </p>
           </div>
 
           <div className="w-full space-y-2">
-            <FormLabel htmlFor="versusToken">Versus Token</FormLabel>
-            <FormField
-              control={form.control}
-              name="versusToken"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input
-                      placeholder="0x"
-                      type="text"
-                      autoComplete="off"
-                      className="w-full"
-                      background="primary"
-                      minLength={1}
-                      textSize="md"
-                      step="any"
-                      {...field}
-                    ></Input>
-                  </FormControl>
-                </FormItem>
-              )}
-            />
+            <TokenInput name="versusToken" title="Versus Token" />
+            <p
+              data-active={formData.versusToken.length > 0 ? "true" : "false"}
+              className="text-red-400 text-sm data-[active=false]:hidden"
+            >
+              {form.formState.errors.versusToken?.message}
+            </p>
           </div>
 
           <div className="w-full space-y-2">
@@ -86,14 +68,14 @@ export default function CreateVaultForm() {
                       autoComplete="off"
                       className="w-full"
                       background="primary"
-                      pattern="^[0-9]*[.,]?[0-9]*$"
+                      // pattern="^[0-9]*[.,]?[0-9]*$"
                       minLength={1}
-                      textSize="md"
-                      inputMode="decimal"
+                      textSize="sm"
+                      // inputMode="decimal"
                       step="any"
                       {...field}
                       onChange={(e) => {
-                        const pattern = /^[0-9]*[.,]?[0-9]*$/;
+                        const pattern = /^-?[0-9]*[.,]?[0-9]*$/;
                         if (pattern.test(e.target.value))
                           return field.onChange(e.target.value);
                       }}
@@ -102,15 +84,61 @@ export default function CreateVaultForm() {
                 </FormItem>
               )}
             />
+
+            {
+              <p className="text-red-400 text-sm">
+                {form.formState.errors.leverageTier?.message}
+              </p>
+            }
           </div>
         </div>
 
         <div className="flex justify-center">
-          <Button disabled={!isValid} variant={"submit"} type="submit">
+          <Button
+            disabled={!form.formState.isValid}
+            variant={"submit"}
+            type="submit"
+          >
             Create
           </Button>
         </div>
       </form>
     </FormProvider>
+  );
+}
+
+function TokenInput({
+  title,
+  name,
+}: {
+  title: string;
+  name: TCreateVaultKeys;
+}) {
+  const form = useFormContext();
+  return (
+    <>
+      <FormLabel htmlFor="longToken">{title}</FormLabel>
+      <FormField
+        control={form.control}
+        name={name}
+        render={({ field }) => (
+          <FormItem>
+            <FormControl>
+              <Input
+                type="text"
+                placeholder="0x"
+                autoComplete="off"
+                className="w-full"
+                background="primary"
+                minLength={1}
+                textSize="sm"
+                step="any"
+                {...field}
+              ></Input>
+            </FormControl>
+          </FormItem>
+        )}
+      />
+    </>
   );
 }
