6adef0eacf14c8206765dcbf8262ca6593505063
Tarp <travistarpley0@gmail.com>
2024-08-11 15:21:05 -0700

    fix: put burn table in card container

--- src/components/portfolio/portfolioPage.tsx
+++ src/components/portfolio/portfolioPage.tsx
@@ -1,10 +1,17 @@
 import React from "react";
 import BurnTable from "./burnTable/burnTable";
+import { Card } from "../ui/card";
+import { Container } from "../ui/container";
 
 export default function PortfolioPage() {
+  const isApe = false;
   return (
     <div>
-      <BurnTable isApe={false} />
+      <Container>
+        <Card>
+          <BurnTable isApe={isApe} />
+        </Card>
+      </Container>
     </div>
   );
 }
