336f5a518fee3c9128f638c54e39f3bfcb8a5243
Tarp <travistarpley0@gmail.com>
2024-08-11 15:17:11 -0700

    refactor: dash -> port

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,7 +1,9 @@
+import PortfolioPage from "@/components/portfolio/portfolioPage";
+
 export default async function Home() {
   return (
     <main className="flex flex-col items-center justify-center text-white">
-      <div></div>
+      <PortfolioPage />
     </main>
   );
 }
