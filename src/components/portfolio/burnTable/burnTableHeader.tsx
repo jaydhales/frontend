a44f9d79cd814d3036b256a1f345b62214c1b2cb
Tarp <travistarpley0@gmail.com>
2024-08-12 12:26:37 -0700

    fix: removed colons

--- src/components/portfolio/burnTable/burnTableHeader.tsx
+++ src/components/portfolio/burnTable/burnTableHeader.tsx
@@ -1,11 +1,11 @@
 export default function BurnTableHeaders() {
   return (
-    <tr className="grid gap-x-4 grid-cols-5 text-left text-gray">
-      <th>Token:</th>
-      <th>Long:</th>
-      <th>Versus:</th>
-      <th>Leverage ratio:</th>
-      <th>Balance:</th>
+    <tr className="grid gap-x-4 grid-cols-5 text-left text-gray border-b border-white pb-1 border-opacity-20">
+      <th>Token</th>
+      <th>Long</th>
+      <th>Versus</th>
+      <th>Leverage ratio</th>
+      <th>Balance</th>
     </tr>
   );
 }
