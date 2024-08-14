e2d51321297147c4077641ce6285e3950c218f13
Tarp <travistarpley0@gmail.com>
2024-08-13 19:59:11 -0700

    fix: select styles

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -13,7 +13,7 @@ const SelectGroup = SelectPrimitive.Group;
 
 const SelectValue = SelectPrimitive.Value;
 const selectTriggerVariants = cva(
-  "flex h-10 w-full items-center justify-between rounded-md   px-3 py-2 placeholder:text-muted-foreground text-sm" +
+  "flex h-10 w-full items-center justify-between rounded-md px-3 py-2 placeholder:text-muted-foreground text-sm" +
     "focus:outline-none focus-visible:outline-none focus-visible:ring-offset-background focus:ring-offset-0  focus:ring focus:ring-ring  disabled:cursor-not-allowed  [&>span]:line-clamp-1",
   {
     variants: {
@@ -90,7 +90,7 @@ const SelectContent = React.forwardRef<
     <SelectPrimitive.Content
       ref={ref}
       className={cn(
-        "relative z-50 max-h-96 min-w-[8rem] overflow-hidden rounded-md border-2 border-gray bg-secondary text-white shadow-md" +
+        "relative z-50 bg-black max-h-96 min-w-[8rem] overflow-hidden rounded-md border-2 border-gray  text-white shadow-md" +
           " data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0" +
           " data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2" +
           " data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
