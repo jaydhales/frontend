24099883778aa24689cde9e2b60be5e9157c7452
Tarp <travistarpley0@gmail.com>
2024-08-27 13:45:11 -0700

    feat: Adding transaction styles

--- src/components/leverage-liquidity/mintForm/transactionModal.tsx
+++ src/components/leverage-liquidity/mintForm/transactionModal.tsx
@@ -1,6 +1,8 @@
 import { AlertDialog, AlertDialogContent } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
+import { X } from "lucide-react";
 
+import type { ReactNode } from "react";
 import { useMemo } from "react";
 interface Props {
   waitForSign: boolean;
@@ -8,6 +10,7 @@ interface Props {
   isTxSuccess: boolean;
   setOpen: (b: boolean) => void;
   open: boolean;
+  children: ReactNode;
 }
 export default function TransactionModal({
   waitForSign,
@@ -15,6 +18,7 @@ export default function TransactionModal({
   isTxSuccess,
   open,
   setOpen,
+  children,
 }: Props) {
   const data = useMemo(() => {
     if (isTxSuccess) {
@@ -26,7 +30,7 @@ export default function TransactionModal({
     if (isTxPending) {
       return { message: "Pending..." };
     }
-    return {};
+    return { message: "Confirm Mint" };
   }, [waitForSign, isTxPending, isTxSuccess]);
   return (
     <AlertDialog open={open} onOpenChange={setOpen}>
@@ -37,20 +41,45 @@ export default function TransactionModal({
         className=" overflow-hidden bg-transparent"
       >
         <div
-          className={`space-y-3 rounded-md border-4  transition-all duration-700 ${data.success ? "border-accent" : "border-accent/0"} bg-secondary p-8 text-white`}
+          className={`rounded-xl relative transition-all duration-700  bg-secondary text-white`}
         >
-          <h2 className="pb-1 text-center font-lora text-xl">{data.message}</h2>
-          <h3 className="text-gray">Lorem ipsum</h3>
-          <Button
-            role="button"
-            onClick={() => setOpen(false)}
-            variant="submit"
-            type="button"
-          >
-            Close
-          </Button>
+          <div className="absolute right-3 top-3">
+            <X
+              className="cursor-pointer"
+              size={20}
+              onClick={() => {
+                setOpen(false);
+              }}
+            />
+          </div>
+          <div className="px-6 bg-black/15 py-4">
+            <h2 className="text-left font-lora text-lg">{data.message}</h2>
+            <div className="flex gap-x-2 py-2">
+              <h3 className="">ETH</h3>
+              <span className="text-gray">{"->"}</span>
+              <h3>APE</h3>
+            </div>
+          </div>
+          <div className="flex py-4 px-6  w-full flex-col gap-y-4 items-center">
+            <div className="flex flex-col w-full py-2 gap-y-1">
+              <StatRow />
+              <StatRow />
+              <StatRow />
+            </div>
+
+            {children}
+          </div>
         </div>
       </AlertDialogContent>
     </AlertDialog>
+  );
+}
+
+function StatRow() {
+  return (
+    <div className="flex justify-between text-[13px]">
+      <h3 className="text-gray ">Price</h3>
+      <h4>398</h4>
+    </div>
   );
 }
