171870a12e93abc5c4f439a8bd390953492a8f02
Tarp <travistarpley0@gmail.com>
2024-08-28 18:55:59 -0700

    feat(transaction-modal): Moved stats to children, Added fee calculation

--- src/components/leverage-liquidity/mintForm/transactionModal.tsx
+++ src/components/leverage-liquidity/mintForm/transactionModal.tsx
@@ -1,4 +1,5 @@
 import { AlertDialog, AlertDialogContent } from "@/components/ui/alert-dialog";
+import ToolTip from "@/components/ui/tooltip";
 import { formatNumber } from "@/lib/utils";
 import { X } from "lucide-react";
 
@@ -12,7 +13,10 @@ interface Props {
   setOpen: (b: boolean) => void;
   open: boolean;
   button: ReactNode;
+  stats: ReactNode;
   quoteData: bigint | undefined;
+  depositAmt: string | undefined;
+  depositAsset: string;
 }
 export default function TransactionModal({
   waitForSign,
@@ -22,6 +26,8 @@ export default function TransactionModal({
   setOpen,
   button,
   quoteData,
+  depositAmt,
+  stats,
 }: Props) {
   const data = useMemo(() => {
     if (isTxSuccess) {
@@ -42,7 +48,7 @@ export default function TransactionModal({
         align="center"
         animate="none"
         closeColor={"black"}
-        className=" overflow-hidden bg-transparent"
+        className="bg-transparent"
       >
         <div
           className={`rounded-xl relative transition-all duration-700  bg-secondary text-white`}
@@ -56,10 +62,13 @@ export default function TransactionModal({
               }}
             />
           </div>
-          <div className="px-6 bg-black/15 pt-5 pb-6">
+          <div className="px-6 rounded-tr-xl rounded-tl-xl bg-black/15 pt-5 pb-6">
             <h2 className="text-left font-lora text-lg">{data.message}</h2>
             <div className="flex gap-x-2 py-2">
-              <h3 className="">ETH</h3>
+              <h3 className="space-x-1">
+                <span>{depositAmt}</span>
+                <span className="text-gray-300 text-sm">ETH</span>
+              </h3>
               <span className="text-gray-500">{"->"}</span>
               <h3 className="space-x-1">
                 <span>
@@ -68,17 +77,12 @@ export default function TransactionModal({
                     4,
                   )}
                 </span>
-                <span className="text-gray text-sm">APE</span>
+                <span className="text-gray-300 text-sm">APE</span>
               </h3>
             </div>
           </div>
           <div className="flex py-4 px-6  w-full flex-col gap-y-4 items-center">
-            <div className="flex flex-col w-full py-2 gap-y-1">
-              <StatRow title={"Price"} value={"Val"} />
-              <StatRow title={"Price"} value={"Val"} />
-              <StatRow title={"Price"} value={"Val"} />
-            </div>
-
+            <div className="flex flex-col w-full py-2 gap-y-1">{stats}</div>
             {button}
           </div>
         </div>
@@ -87,10 +91,20 @@ export default function TransactionModal({
   );
 }
 
-function StatRow({ title, value }: { title: string; value: string }) {
+export function TransactionModalStat({
+  title,
+  value,
+}: {
+  title: string;
+  value: string;
+}) {
   return (
-    <div className="flex justify-between text-[13px]">
-      <h3 className="text-gray-500 ">{title}</h3>
+    <div className="flex justify-between relative text-[13px]">
+      <h3 className="text-gray-300 ">
+        <span className="flex z-20 items-center gap-x-1">
+          {title} <ToolTip>Fee Info</ToolTip>
+        </span>
+      </h3>
       <h4>{value}</h4>
     </div>
   );
