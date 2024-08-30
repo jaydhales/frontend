82306d28fd067c55f84bc5e3113f45218fbf7d7a
Tarp <travistarpley0@gmail.com>
2024-08-29 18:03:33 -0700

    refactor: made transactionModal reusable

--- src/components/shared/transactionModal.tsx
+++ src/components/shared/transactionModal.tsx
@@ -1,14 +1,8 @@
 import { AlertDialog, AlertDialogContent } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
 import ToolTip from "@/components/ui/tooltip";
-import type { TMintFormFields } from "@/lib/types";
-import { formatNumber } from "@/lib/utils";
 import { X } from "lucide-react";
-
 import type { ReactNode } from "react";
-import { useMemo } from "react";
-import { useFormContext } from "react-hook-form";
-import { formatUnits } from "viem";
 interface Props {
   setOpen: (b: boolean) => void;
   open: boolean;
@@ -43,77 +37,29 @@ function StatSubmitContainer({ children }: { children: ReactNode }) {
     </div>
   );
 }
-interface InfoProps {
-  waitForSign: boolean;
-  isTxPending: boolean;
-  isTxSuccess: boolean;
-  quoteData: bigint | undefined;
-  usingEth: boolean;
-}
 
-function Info({
-  waitForSign,
-  usingEth,
-  isTxPending,
-  isTxSuccess,
-  quoteData,
-}: InfoProps) {
-  const data = useMemo(() => {
-    if (isTxSuccess) {
-      return { message: "Success!", success: true };
-    }
-    if (waitForSign) {
-      return { message: "Please Sign Transaction." };
-    }
-    if (isTxPending) {
-      return { message: "Pending..." };
-    }
-    return { message: "Confirm Mint" };
-  }, [waitForSign, isTxPending, isTxSuccess]);
-
-  const form = useFormContext<TMintFormFields>();
-  const collateralAssetName = usingEth
-    ? "ETH"
-    : form.getValues("long").split(",")[1] ?? "";
-  const deposit = form.getValues("deposit");
-
+function InfoContainer({ children }: { children: ReactNode }) {
   return (
     <div className="px-6 rounded-tr-xl rounded-tl-xl bg-black/15 pt-5 pb-6">
-      <h2 className="text-left font-lora text-lg">{data.message}</h2>
-      <div className="flex gap-x-2 py-2">
-        <h3 className="space-x-1">
-          <span>{deposit}</span>
-          <span className="text-gray-300 text-sm">{collateralAssetName}</span>
-        </h3>
-        <span className="text-gray-500">{"->"}</span>
-        <h3 className="space-x-1">
-          <span>
-            {formatNumber(parseFloat(formatUnits(quoteData ?? 0n, 18)), 4)}
-          </span>
-          <span className="text-gray-300 text-sm">APE</span>
-        </h3>
-      </div>
-      <div className="text-[12px] italic text-gray-400 items-center pt-2 w-[300px]">
-        <span>Output is estimated.</span>
-      </div>
+      {children}
     </div>
   );
 }
 
-function Close({
-  reset,
-  setOpen,
-}: {
-  setOpen: (b: boolean) => void;
-  reset: () => void;
-}) {
+function Disclaimer({ children }: { children: ReactNode }) {
+  return (
+    <div className="text-[12px] italic text-gray-400 items-center pt-2 w-[300px]">
+      <span>{children}</span>
+    </div>
+  );
+}
+function Close({ setOpen }: { setOpen: (b: boolean) => void }) {
   return (
     <div className="absolute right-3 top-3">
       <X
         className="cursor-pointer"
         size={20}
         onClick={() => {
-          reset();
           setOpen(false);
         }}
       />
@@ -149,11 +95,12 @@ function SubmitButton({
 }
 const TransactionModal = {
   Root,
-  Info,
+  InfoContainer,
   Close,
   StatRow,
   StatSubmitContainer,
   StatContainer,
+  Disclaimer,
   SubmitButton,
 };
 export default TransactionModal;
