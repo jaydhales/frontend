df7b34922a6776e48a0ecf1246e7a78e165e992e
Thai Nguyen <jooohn.eth@gmail.com>
2024-09-02 20:59:25 -0400

    temp: replace leverage page as auction page before nav link ready

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,22 @@ import LeverageLiquidityContent from "@/components/leverage-liquidity/leverageLi
 import LeverageLiquidityPage from "@/components/leverage-liquidity/leverageLiquidityPage";
 import MintForm from "@/components/leverage-liquidity/mintForm/mintForm";
 
+import AuctionPage from "@/components/auction/auctionPage";
+
 import { api } from "@/trpc/server";
 
 export default async function Home() {
   const vaultQuery = await api.vault.getVaults();
   return (
     <main className="flex flex-col items-center justify-center text-white">
-      <LeverageLiquidityPage title={"Take on Leverage"}>
+      {/* <LeverageLiquidityPage title={"Take on Leverage"}>
         <LeverageLiquidityContent
           isApe
           form={<MintForm isApe vaultsQuery={vaultQuery} />}
           vaultsQuery={vaultQuery}
         ></LeverageLiquidityContent>
-      </LeverageLiquidityPage>
+      </LeverageLiquidityPage> */}
+      <AuctionPage />
     </main>
   );
 }
