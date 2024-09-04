9f663dec8be5c688cc5f5d42a649eaffed5c2878
tarp <travistarpley0@gmail.com>
2024-09-03 18:00:24 -0700

    feat: Added sir to portfolio

--- src/components/portfolio/claimCard.tsx
+++ src/components/portfolio/claimCard.tsx
@@ -17,23 +17,22 @@ export default function ClaimCard() {
     { enabled: isConnected },
   );
   return (
-    <Card className="border border-secondary-300">
+    <div className=" border-secondary-300">
       <ClaimModal open={openModal} setOpen={setOpenModal} />
-      <h2 className="flex text-gray-200 gap-x-1 pb-1 items-center text-sm ">
-        <span>Claimable Value</span>
-      </h2>
-      <div className="pt-2"></div>
-      <div className="bg-secondary-300 px-2 py-4 rounded-md text-2xl">
+      <div className="bg-secondary-300 px-2 py-2 rounded-md text-2xl">
+        <h2 className="flex text-gray-200 gap-x-1 pb-1 items-center text-sm ">
+          <span>Claimable Dividends</span>
+        </h2>
         <div className="flex items-center justify-between">
           <h3 className="text-3xl">
-            {formatEther(dividends ?? 0n)}{" "}
-            <span className="text-gray-500 text-[14px]">SIR</span>
+            {formatEther(dividends ?? 0n)}
+            <span className="text-gray-500 text-sm"> ETH</span>
           </h3>
           <Button onClick={() => setOpenModal(true)} className="py-2">
             Claim
           </Button>
         </div>
       </div>
-    </Card>
+    </div>
   );
 }
