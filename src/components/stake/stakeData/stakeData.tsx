8d515d97bda3202c3a235d473503b462e74b7b82
Thai Nguyen <jooohn.eth@gmail.com>
2024-09-08 15:33:30 -0400

    feat: uncomment tvl changes

--- src/components/stake/stakeData/stakeData.tsx
+++ src/components/stake/stakeData/stakeData.tsx
@@ -33,10 +33,25 @@ const StakeData = () => {
     <div className="flex justify-evenly py-[24px]">
       <div className="bg-secondary rounded-md w-[47%] py-2 flex flex-col justify-center items-center gap-2">
         <div className="text-sm font-medium">Total SIR Locked</div>
-        <div className="text-2xl font-semibold font-lora">
+        {/* <div className="text-2xl font-semibold font-lora">
           {parseFloat(formatUnits(totalValueLocked ?? 0n, 12)).toFixed(4)}
+        </div> */}
+        <div className="text-2xl font-semibold font-lora">
+          {(() => {
+            const value = parseFloat(formatUnits(totalValueLocked ?? 0n, 12));
+            if (value >= 1e9) {
+              return (value / 1e9).toFixed(2) + "B";
+            } else if (value >= 1e6) {
+              return (value / 1e6).toFixed(2) + "M";
+            } else if (value >= 1e3) {
+              return Math.floor(value).toLocaleString();
+            } else {
+              return value.toFixed(2);
+            }
+          })()}
         </div>
       </div>
+
       <div className="bg-secondary rounded-md w-[47%] py-2 flex flex-col justify-center items-center gap-2">
         <div className="flex flex-row w-full items-center justify-center">
           <div className="text-sm font-medium px-2">Staking APR</div>
