dfe6d53863507f95877fe3a7761ea777cb691f34
Tarp <travistarpley0@gmail.com>
2024-09-11 18:33:59 -0700

    feat: Adding transaction modal to unstakeForm

--- src/components/stake/unstakeForm/unstakeForm.tsx
+++ src/components/stake/unstakeForm/unstakeForm.tsx
@@ -5,15 +5,13 @@ import { Form, FormLabel } from "@/components/ui/form";
 import { Button } from "@/components/ui/button";
 
 import { useFormContext } from "react-hook-form";
-import { useAccount } from "wagmi";
+import { useAccount, useWaitForTransactionReceipt } from "wagmi";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
 
 import UnstakeInput from "./unstakeInput";
 import type { TUnstakeFormFields } from "@/lib/types";
 import ClaimFeesCheckbox from "@/components/stake/unstakeForm/claimFeesCheck";
-// import GasFeeEstimation from "@/components/shared/gasFeeEstimation";
-// import { useEffect } from "react";
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
 import { type SimulateContractReturnType, parseUnits, formatUnits } from "viem";
 import { z } from "zod";
@@ -24,6 +22,8 @@ import { SirContract } from "@/contracts/sir";
 
 import useUnstakeError from "@/components/stake/hooks/useUnstakeError";
 import { useCheckSubmitValid } from "@/components/leverage-liquidity/mintForm/hooks/useCheckSubmitValid";
+import TransactionModal from "@/components/shared/transactionModal";
+import { TransactionStatus } from "@/components/leverage-liquidity/mintForm/transactionStatus";
 
 type SimulateReq = SimulateContractReturnType["request"] | undefined;
 interface Props {
@@ -58,8 +58,8 @@ const UnstakeForm = ({
   });
 
   const { writeContract, data: hash, isPending } = useWriteContract();
-  // const { isLoading: isConfirming, isSuccess: isConfirmed } =
-  //   useWaitForTransactionReceipt({ hash });
+  const { isLoading: isConfirming, isSuccess: isConfirmed } =
+    useWaitForTransactionReceipt({ hash });
 
   const { isValid, errorMessage } = useCheckSubmitValid({
     deposit: safeAmount.success ? safeAmount.data.toString() : "0",
@@ -94,17 +94,39 @@ const UnstakeForm = ({
     errorMessage,
     rootErrorMessage: form.formState.errors.root?.message,
   });
-
+  const [open, setOpen] = useState(false);
+  let submitButtonText = "Confirm Unstake";
+  if (isPending || isConfirming) {
+    submitButtonText = "Pending...";
+  }
+  if (isConfirmed) {
+    submitButtonText = "Close";
+  }
   return (
     <>
-      {/* <ProgressAlert */}
-      {/*   isTxSuccess={isConfirmed} */}
-      {/*   isTxPending={isConfirming} */}
-      {/*   waitForSign={isPending} */}
-      {/* /> */}
       <Card className="w-full ">
+        <TransactionModal.Root setOpen={setOpen} open={open}>
+          <TransactionModal.InfoContainer>
+            <TransactionStatus
+              waitForSign={isPending}
+              isTxPending={isConfirming}
+            />
+          </TransactionModal.InfoContainer>
+          <TransactionModal.StatSubmitContainer>
+            <TransactionModal.SubmitButton
+              onClick={() => {
+                onSubmit();
+              }}
+              loading={isPending || isConfirming}
+              disabled={isValid}
+            >
+              {submitButtonText}
+            </TransactionModal.SubmitButton>
+          </TransactionModal.StatSubmitContainer>
+        </TransactionModal.Root>
+
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)}>
+          <form>
             <UnstakeInput
               form={form}
               balance={formatUnits(balance ?? 0n, 12)}
@@ -117,10 +139,17 @@ const UnstakeForm = ({
 
             <div className=" flex-col flex items-center justify-center mt-[20px]">
               {address && (
-                <Button variant={"submit"} type="submit" disabled={!isValid}>
-                  {form.formState.errors.root?.message
-                    ? form.formState.errors.root?.message.toString()
-                    : "Unstake"}
+                <Button
+                  variant={"submit"}
+                  onClick={() => {
+                    if (isValid) {
+                      setOpen(true);
+                    }
+                  }}
+                  type="submit"
+                  disabled={!isValid}
+                >
+                  Unstake
                 </Button>
               )}
               {!address && (
@@ -132,13 +161,13 @@ const UnstakeForm = ({
                   Connect Wallet
                 </Button>
               )}
-              {/* {form.formState.errors.root?.message && (
-              <div className="w-[450px] pt-[20px] flex justify-center items-center">
-                <p className="h-[20px] text-center text-sm text-red-400">
-                  {address && <>{form.formState.errors.root?.message}</>}
-                </p>
-              </div>
-            )} */}
+              {form.formState.errors.root?.message && (
+                <div className="w-[450px] pt-[20px] flex justify-center items-center">
+                  <p className="h-[20px] text-center text-sm text-red-400">
+                    {address && <>{form.formState.errors.root?.message}</>}
+                  </p>
+                </div>
+              )}
             </div>
           </form>
         </Form>
