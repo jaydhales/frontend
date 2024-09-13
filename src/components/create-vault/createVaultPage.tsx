5f2c5f217c47c24ce0b6602943792df56de2af2c
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-09-12 21:01:07 -0700

    fix: rm lorem ipsum

--- src/components/create-vault/createVaultPage.tsx
+++ src/components/create-vault/createVaultPage.tsx
@@ -7,12 +7,6 @@ export default function CreateVaultPage() {
     <div>
       <PageHeader> Create Vault</PageHeader>
       <div className="w-[600px] flex flex-col items-center">
-        <h2 className="text-center py-2">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur
-          dolorem, quisquam perferendis tenetur quod nam quia dolorum voluptate!
-          Totam similique incidunt labore, in vero suscipit cum pariatur ad
-          veritatis impedit.
-        </h2>
         <br />
         <Card className="md:w-[450px]">
           <CreateVaultForm />
