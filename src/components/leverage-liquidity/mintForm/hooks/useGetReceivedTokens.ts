6d41e05afad1bb8af862b7a4afc00f809f877861
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-09-12 21:32:10 -0700

    feat: Added received amount for tea

--- src/components/leverage-liquidity/mintForm/hooks/useGetReceivedTokens.ts
+++ src/components/leverage-liquidity/mintForm/hooks/useGetReceivedTokens.ts
@@ -1,34 +1,62 @@
 import { ApeContract } from "@/contracts/ape";
+import { VaultContract } from "@/contracts/vault";
 import type { TAddressString } from "@/lib/types";
 import { useEffect, useState } from "react";
 import { decodeEventLog, type Log } from "viem";
 interface Props {
   logs: Log<bigint, number, false>[] | undefined;
   apeAddress: TAddressString;
+  isApe: boolean;
 }
 
 /**
  * Get tokens received from transaction logs.
  */
-export function useGetReceivedTokens({ logs, apeAddress }: Props) {
+export function useGetReceivedTokens({ logs, apeAddress, isApe }: Props) {
   const [tokenReceived, setTokenReceived] = useState<bigint | undefined>();
   useEffect(() => {
+    console.log(logs, "LOGS", isApe, "isAPE");
     if (logs) {
-      const foundLogs = logs.filter((l) => l.address === apeAddress);
-      if (!foundLogs) return;
-      foundLogs.forEach((log) => {
-        const parsed = decodeEventLog({
-          abi: ApeContract.abi,
-          data: log.data,
-          topics: log.topics,
-        });
+      if (isApe) {
+        const foundLogs = logs.filter(
+          (l) => l.address.toLowerCase() === apeAddress.toLowerCase(),
+        );
+        if (!foundLogs) return;
+        foundLogs.forEach((log) => {
+          const parsed = decodeEventLog({
+            abi: ApeContract.abi,
+            data: log.data,
+            topics: log.topics,
+          });
 
-        if (parsed.eventName === "Transfer") {
-          setTokenReceived(parsed.args.amount);
-          return;
-        }
-      });
+          if (parsed.eventName === "Transfer") {
+            setTokenReceived(parsed.args.amount);
+            return;
+          }
+        });
+      } else {
+        console.log(logs, VaultContract.address);
+        const foundLogs = logs.filter(
+          (l) =>
+            l.address.toLowerCase() === VaultContract.address.toLowerCase(),
+        );
+        console.log(foundLogs);
+        if (!foundLogs) return;
+        foundLogs.forEach((log) => {
+          const parse = decodeEventLog({
+            abi: VaultContract.abi,
+            data: log.data,
+            topics: log.topics,
+          });
+          console.log(parse);
+          if (parse.eventName === "Mint") {
+            console.log(parse.args);
+            setTokenReceived(parse.args.collateralIn);
+            return;
+          }
+        });
+      }
     }
-  }, [apeAddress, logs]);
+  }, [apeAddress, isApe, logs]);
   return { tokenReceived };
 }
