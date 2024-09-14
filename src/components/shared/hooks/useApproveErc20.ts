dd4dfb52c666faadd99ded0fb4e49886c31e04b0
Tarp <travistarpley0@gmail.com>
2024-09-14 13:25:14 -0700

    fix: Switched back to old approve, added getAbi function
    abi is computed every render using token address to ensure correct
    approve simulation

--- src/components/shared/hooks/useApproveErc20.ts
+++ src/components/shared/hooks/useApproveErc20.ts
@@ -1,4 +1,3 @@
-import { SafeApproveContract } from "@/contracts/safe-approve";
 import type { TAddressString } from "@/lib/types";
 import { formatUnits, maxInt256, parseUnits } from "viem";
 import { useSimulateContract } from "wagmi";
@@ -9,13 +8,61 @@ interface Props {
 
 export function useApproveErc20({ tokenAddr, approveContract }: Props) {
   const approveSimulate = useSimulateContract({
-    ...SafeApproveContract,
-    functionName: "approveToken",
-    args: [
-      tokenAddr as TAddressString,
-      approveContract,
-      parseUnits(formatUnits(maxInt256, 18), 0),
-    ],
+    address: tokenAddr as TAddressString,
+    abi: getAbi(tokenAddr as TAddressString),
+    functionName: "approve",
+    args: [approveContract, parseUnits(formatUnits(maxInt256, 18), 0)],
   });
   return { approveSimulate };
 }
+
+function getAbi(tokenAddr: TAddressString) {
+  if (
+    tokenAddr.toLowerCase() ===
+    "0xdAC17F958D2ee523a2206206994597C13D831ec7".toLowerCase()
+  ) {
+    return nonStandardAbi;
+  } else {
+    return abi;
+  }
+}
+const abi = [
+  {
+    type: "function",
+    name: "approve",
+    stateMutability: "nonpayable",
+    inputs: [
+      {
+        name: "spender",
+        type: "address",
+      },
+      {
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    outputs: [
+      {
+        type: "bool",
+      },
+    ],
+  },
+];
+const nonStandardAbi = [
+  {
+    type: "function",
+    name: "approve",
+    stateMutability: "nonpayable",
+    inputs: [
+      {
+        name: "spender",
+        type: "address",
+      },
+      {
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    outputs: [],
+  },
+];
