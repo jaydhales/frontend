445fceff75d8410a96c3055b3477b703c4eff4e6
Tarp <travistarpley0@gmail.com>
2024-09-13 18:04:13 -0700

    feat: Added support for non-standard erc20 approvals

--- src/components/shared/hooks/useApproveErc20.ts
+++ src/components/shared/hooks/useApproveErc20.ts
@@ -1,5 +1,6 @@
 import type { TAddressString } from "@/lib/types";
-import { erc20Abi, formatUnits, maxInt256, parseUnits } from "viem";
+import { useEffect, useState } from "react";
+import { formatUnits, maxInt256, parseUnits } from "viem";
 import { useSimulateContract } from "wagmi";
 interface Props {
   tokenAddr: string;
@@ -7,14 +8,76 @@ interface Props {
 }
 
 export function useApproveErc20({ tokenAddr, approveContract }: Props) {
+  const [nonStandardErc20, setNonStandardErc20] = useState(false);
   const approveSimulate = useSimulateContract({
     address: tokenAddr as TAddressString,
-    abi: erc20Abi,
+    abi: nonStandardErc20 ? nonStandardAbi : abi,
     functionName: "approve",
     args: [approveContract, parseUnits(formatUnits(maxInt256, 18), 0)],
   });
-  if (approveSimulate.error) {
-    console.log(approveSimulate.error);
-  }
+  console.log(nonStandardErc20, "non standard");
+  useEffect(() => {
+    if (approveSimulate.error) {
+      console.log(
+        approveSimulate.error.message,
+        "ERROR APPROVE",
+        "ContractFunctionZeroDataError",
+      );
+      // ContractFunctionExecutionError
+      if (
+        approveSimulate.error.message.includes(
+          `The contract function "approve" returned no data ("0x").`,
+        )
+      ) {
+        console.log(approveSimulate.error, "APPROVE SIMULATE ERROR");
+        setNonStandardErc20(true);
+      } else {
+        setNonStandardErc20(false);
+      }
+    } else {
+      setNonStandardErc20(false);
+    }
+  }, [approveSimulate.error]);
   return { approveSimulate };
 }
+
+const abi = [
+  {
+    type: "function",
+    name: "approve",
+    stateMutability: "nonpayable",
+    inputs: [
+      {
+        name: "spender",
+        type: "address",
+      },
+      {
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    outputs: [
+      {
+        type: "bool",
+      },
+    ],
+  },
+];
+const nonStandardAbi = [
+  {
+    type: "function",
+    name: "approve",
+    stateMutability: "nonpayable",
+    inputs: [
+      {
+        name: "spender",
+        type: "address",
+      },
+      {
+        name: "amount",
+        type: "uint256",
+      },
+    ],
+    outputs: [],
+  },
+];
