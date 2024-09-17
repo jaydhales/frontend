0fa79f2f50e13b1a0a932761c72e27e799a39948
Tarp <travistarpley0@gmail.com>
2024-09-17 09:13:39 -0700

    fix: add env rpc to reverse proxy rpc

--- next.config.mjs
+++ next.config.mjs
@@ -6,24 +6,24 @@
 
 /* @type {import("next").NextConfig} */
 const config = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "raw.githubusercontent.com",
-                port: "",
-                pathname: "/**",
-            },
-        ],
-    },
-    async rewrites() {
-        return [
-            {
-                source: "/api/rpc",
-                destination: "http://140.82.62.189:8545",
-            },
-        ];
-    },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/rpc",
+        destination: process.env.RPC_URL,
+      },
+    ];
+  },
 };
 
 export default config;
