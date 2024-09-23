588c6b5cf2b8f8f7bf36a4b789816006542e5253
Tarp <travistarpley0@gmail.com>
2024-09-23 11:32:43 -0700

    feat: removed unstake and claim tabs

--- src/components/stake/stakeTabs.tsx
+++ src/components/stake/stakeTabs.tsx
@@ -36,20 +36,6 @@ const StakeTabs = () => {
     { enabled: isConnected },
   );
 
-  const { data: totalBalance } = api.user.getTotalSirBalance.useQuery(
-    {
-      user: address,
-    },
-    { enabled: isConnected },
-  );
-
-  const { data: dividends } = api.user.getDividends.useQuery(
-    {
-      staker: address,
-    },
-    { enabled: isConnected },
-  );
-
   const { data: ethBalance } = api.user.getEthBalance.useQuery(
     {
       userAddress: address,
@@ -59,80 +45,19 @@ const StakeTabs = () => {
     },
   );
 
-  const safeTotalBalance = useMemo(() => {
-    return z.coerce.bigint().default(0n).safeParse(totalBalance);
-  }, [totalBalance]);
-  console.log(safeTotalBalance, "safe total balance");
-  const safeDividends = useMemo(() => {
-    return z.coerce.bigint().default(0n).safeParse(dividends);
-  }, [dividends]);
-
-  useEffect(() => {
-    console.log("---TABS---");
-    console.log(`Unstaked SIR: ${balance?.tokenBalance?.result}`);
-    console.log(`Unstaked SIR Allowance: ${balance?.tokenAllowance?.result}`);
-    console.log(
-      `Staked SIR: ${balance?.tokenBalance?.result && safeTotalBalance.success && safeTotalBalance.data - balance?.tokenBalance?.result}`,
-    );
-    console.log(
-      `ETH Balance: ${parseFloat(formatEther(ethBalance ?? 0n)).toFixed(4)}`,
-    );
-  }, [balance, safeTotalBalance, safeDividends, ethBalance]);
-
-  const { Claim, isFetching: claimFetching } = useClaim();
-
   return (
-    <Tabs defaultValue="stake">
-      <div className="flex justify-center">
-        <TabsList defaultValue={"stake"}>
-          <TabsTrigger value={"stake"}>Stake</TabsTrigger>
-          <TabsTrigger value={"unstake"}>Unstake</TabsTrigger>
-          <TabsTrigger value={"claimFees"}>Claim fees</TabsTrigger>
-        </TabsList>
-      </div>
+    <div>
       <br />
-      <TabsContent value="stake">
-        <Container>
-          <StakeFormProvider>
-            <StakeForm
-              balance={balance?.tokenBalance?.result}
-              allowance={balance?.tokenAllowance?.result}
-              ethBalance={ethBalance}
-            ></StakeForm>
-          </StakeFormProvider>
-        </Container>
-      </TabsContent>
-      <TabsContent value="unstake">
-        {/* <Container> */}
-        {/*   <UnstakeFormProvider> */}
-        {/*     <UnstakeForm */}
-        {/*       balance={ */}
-        {/*         safeTotalBalance.success && balance?.tokenBalance?.result */}
-        {/*           ? safeTotalBalance.data - balance?.tokenBalance?.result */}
-        {/*           : 0n */}
-        {/*       } */}
-        {/*       dividends={formatEther( */}
-        {/*         safeDividends.success ? safeDividends.data : 0n, */}
-        {/*       )} */}
-        {/*       claimResult={Claim?.result} */}
-        {/*     /> */}
-        {/*   </UnstakeFormProvider> */}
-        {/* </Container> */}
-      </TabsContent>
-      <TabsContent value="claimFees">
-        <Container>
-          <ClaimFees
-            ethBalance={formatEther(ethBalance ?? 0n)}
-            claimAmount={formatEther(
-              safeDividends.success ? safeDividends.data : 0n,
-            )}
-            claimSimulate={Claim?.request as SimulateReq}
-            claimResult={Claim?.result}
-            claimFetching={claimFetching}
-          ></ClaimFees>
-        </Container>
-      </TabsContent>
-    </Tabs>
+      <Container>
+        <StakeFormProvider>
+          <StakeForm
+            balance={balance?.tokenBalance?.result}
+            allowance={balance?.tokenAllowance?.result}
+            ethBalance={ethBalance}
+          ></StakeForm>
+        </StakeFormProvider>
+      </Container>
+    </div>
   );
 };
 
