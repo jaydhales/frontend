efbd400c4cd13bda0aa83a83a3233822e20e39a2
Tarp <travistarpley0@gmail.com>
2024-09-24 16:15:23 -0700

    feat: Added custom connect button

---
+++ src/components/connectButton.tsx
@@ -0,0 +1,82 @@
+"use client";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Image from "next/image";
+import { Button } from "./ui/button";
+export const CustomConnectButton = () => {
+  return (
+    <ConnectButton.Custom>
+      {({
+        account,
+        chain,
+        openAccountModal,
+        openChainModal,
+        openConnectModal,
+        authenticationStatus,
+        mounted,
+      }) => {
+        // Note: If your app doesn't use authentication, you
+        // can remove all 'authenticationStatus' checks
+        const ready = mounted && authenticationStatus !== "loading";
+        const connected =
+          ready &&
+          account &&
+          chain &&
+          (!authenticationStatus || authenticationStatus === "authenticated");
+        return (
+          <div
+            {...(!ready && {
+              "aria-hidden": true,
+              style: {
+                opacity: 0,
+                pointerEvents: "none",
+                userSelect: "none",
+              },
+            })}
+          >
+            {(() => {
+              if (!connected) {
+                return (
+                  <Button
+                    variant="outline"
+                    className="rounded-full text-white"
+                    onClick={openConnectModal}
+                    type="button"
+                  >
+                    Connect Wallet
+                  </Button>
+                );
+              }
+              if (chain.unsupported) {
+                return (
+                  <Button
+                    variant={"outline"}
+                    className="rounded-full text-white"
+                    onClick={openChainModal}
+                    type="button"
+                  >
+                    Wrong network
+                  </Button>
+                );
+              }
+              return (
+                <div style={{ display: "flex", gap: 12 }}>
+                  <Button
+                    variant="outline"
+                    className="rounded-full text-white"
+                    onClick={openAccountModal}
+                    type="button"
+                  >
+                    {account.displayName}
+                    {account.displayBalance
+                      ? ` (${account.displayBalance})`
+                      : ""}
+                  </Button>
+                </div>
+              );
+            })()}
+          </div>
+        );
+      }}
+    </ConnectButton.Custom>
+  );
+};
