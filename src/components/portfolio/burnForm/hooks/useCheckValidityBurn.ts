963d08a9958e30b35233b6041f32c6f5c15c5c4e
Tarp <travistarpley0@gmail.com>
2024-09-25 18:32:44 -0700

    feat: add chain check to burn form

--- src/components/portfolio/burnForm/hooks/useCheckValidityBurn.ts
+++ src/components/portfolio/burnForm/hooks/useCheckValidityBurn.ts
@@ -1,13 +1,22 @@
 import { parseUnits } from "viem";
 import type { TBurnFields } from "../burnForm";
+import { useMemo } from "react";
+import useGetChainId from "@/components/shared/hooks/useGetChainId";
+import { env } from "@/env";
 
 export function useCheckValidityBurn(
   formData: TBurnFields,
   tokenBalance: bigint | undefined,
 ) {
-  if (parseUnits(formData.deposit ?? "0", 18) > (tokenBalance ?? 0n)) {
-    return { isValid: false, error: "Insufficent balance." };
-  }
-
-  return { isValid: true, error: null };
+  const chainId = useGetChainId();
+  const { isValid, error } = useMemo(() => {
+    if (chainId?.toString() !== env.NEXT_PUBLIC_CHAIN_ID && Boolean(chainId)) {
+      return { isValid: false, error: "Wrong network!" };
+    }
+    if (parseUnits(formData.deposit ?? "0", 18) > (tokenBalance ?? 0n)) {
+      return { isValid: false, error: "Insufficent balance." };
+    }
+    return { isValid: true, error: null };
+  }, [chainId, formData.deposit, tokenBalance]);
+  return { isValid, error };
 }
