666537f8fc09e4258f6143cc1e2e488938e6650b
Tarp <travistarpley0@gmail.com>
2024-09-26 13:33:43 -0700

    feat: Added more validity checks for burn form

--- src/components/portfolio/burnForm/hooks/useCheckValidityBurn.ts
+++ src/components/portfolio/burnForm/hooks/useCheckValidityBurn.ts
@@ -1,10 +1,21 @@
+import type { SimulateContractReturnType } from "viem";
 import { parseUnits } from "viem";
 import type { TBurnFields } from "../burnForm";
 
 export function useCheckValidityBurn(
   formData: TBurnFields,
   tokenBalance: bigint | undefined,
+  isClaimingRewards: boolean,
+  claimingRewardsRequest: SimulateContractReturnType["request"],
 ) {
+  if (isClaimingRewards) {
+    if (claimingRewardsRequest) {
+      return { isValid: true, error: null };
+    }
+  }
+  if (!formData.deposit) {
+    return { isValid: false, error: null };
+  }
   if (parseUnits(formData.deposit ?? "0", 18) > (tokenBalance ?? 0n)) {
     return { isValid: false, error: "Insufficent balance." };
   }
