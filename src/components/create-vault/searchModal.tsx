212927789121816ba98b5cfac921036728c88dac
Tarp <travistarpley0@gmail.com>
2024-09-26 18:56:16 -0700

    feat: put matches on top of search

--- src/components/create-vault/searchModal.tsx
+++ src/components/create-vault/searchModal.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { AlertDialog, AlertDialogContent } from "../ui/alert-dialog";
 import { api } from "@/trpc/react";
 import { Input } from "../ui/input";
@@ -7,6 +7,13 @@ import ImageWithFallback from "../shared/ImageWithFallback";
 import { getLogoAsset } from "@/lib/utils";
 import type { TAddressString } from "@/lib/types";
 import { useDebounce } from "../shared/hooks/useDebounce";
+interface TRow {
+  ID: number;
+  Address: string;
+  Decimals: number;
+  Name: string;
+  Symbol: string;
+}
 export default function SearchModal({
   setSearchTokensOpen,
   searchTokensOpen,
@@ -21,7 +28,27 @@ export default function SearchModal({
     { searchTerm: searchTermDebounced },
     { enabled: searchTokensOpen && Boolean(searchTerm) },
   );
-  console.log(data.data);
+  const { found, match } = useMemo(() => {
+    let match: TRow[] = [];
+    const found = data.data?.filter((row) => {
+      console.log(row);
+      if (
+        row.Symbol.toLowerCase() !== searchTerm.toLowerCase() &&
+        row.Name.toLowerCase() !== searchTerm.toLowerCase()
+      ) {
+        return true;
+      } else {
+        if (!match) {
+          match = [row];
+        } else {
+          match.push(row);
+        }
+        return false;
+      }
+    });
+    console.log({ found, match }, "HELLLO");
+    return { found, match: match ?? [] };
+  }, [data.data, searchTerm]);
   return (
     <AlertDialog open={searchTokensOpen} onOpenChange={setSearchTokensOpen}>
       <AlertDialogContent
@@ -44,30 +71,35 @@ export default function SearchModal({
           />
           <div className="pt-3"></div>
           <div className="h-[600px] overflow-y-auto space-y-3">
-            {data?.data?.map((row) => {
-              return (
-                <div
-                  className="flex w-full items-center px-2 gap-x-4 rounded-md cursor-pointer hover:bg-secondary-300 justify-between"
-                  key={row.ID}
-                >
-                  <div className="space-x-2">
-                    <span className="text-wrap">{row.Name}</span>
-                    <span className="text-[12px] text-gray-400">
-                      {row.Symbol}
-                    </span>
-                  </div>
-                  <ImageWithFallback
-                    src={getLogoAsset(row.Address as TAddressString)}
-                    alt="collateral"
-                    width={40}
-                    height={40}
-                  />
-                </div>
-              );
+            {match?.map((row) => {
+              return <Row key={row.ID} {...row} />;
+            })}
+            {found?.map((row) => {
+              return <Row key={row.ID} {...row} />;
             })}
           </div>
         </div>
       </AlertDialogContent>
     </AlertDialog>
+  );
+}
+
+function Row(row: TRow) {
+  return (
+    <div
+      className="flex w-full items-center px-2 gap-x-4 rounded-md cursor-pointer hover:bg-secondary-300 justify-between"
+      key={row.ID}
+    >
+      <div className="space-x-2">
+        <span className="text-wrap">{row.Name}</span>
+        <span className="text-[12px] text-gray-400">{row.Symbol}</span>
+      </div>
+      <ImageWithFallback
+        src={getLogoAsset(row.Address as TAddressString)}
+        alt="collateral"
+        width={40}
+        height={40}
+      />
+    </div>
   );
 }
