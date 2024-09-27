e35680eb2c2af912c3cb067d66664d2cd4b98283
Tarp <travistarpley0@gmail.com>
2024-09-27 12:07:34 -0700

    format

--- src/components/portfolio/burnForm/sectionTwo.tsx
+++ src/components/portfolio/burnForm/sectionTwo.tsx
@@ -1,6 +1,5 @@
 import ImageWithFallback from "@/components/shared/ImageWithFallback";
-import { formatBigInt, getLogoAsset } from "@/lib/utils";
-import Image from "next/image";
+import { formatNumber, getLogoAsset } from "@/lib/utils";
 export function SectionTwo({
   bg,
   data,
@@ -8,7 +7,7 @@ export function SectionTwo({
   collateralSymbol,
 }: {
   bg: string;
-  amount: bigint | undefined;
+  amount: string;
   data:
     | {
         leverageTier: number | undefined;
@@ -22,7 +21,7 @@ export function SectionTwo({
     <div className={`w-full  rounded-md ${bg} `}>
       <div className="flex items-end justify-between">
         <div>
-          <h2 className="text-[28px]">{formatBigInt(amount, 4)}</h2>
+          <h2 className="text-[28px]">{formatNumber(amount, 4)}</h2>
         </div>
         <div>
           <div className={"flex  gap-x-2 "}>
