58cb96ddb5fb368bca1f56caf90ff131d5b3e70e
Tarp <travistarpley0@gmail.com>
2024-10-03 18:52:50 -0700

    feat: Added getReserves with vault and reserve caching

---
+++ src/app/leverage/[slug]/page.tsx
@@ -0,0 +1,99 @@
+import LeverageLiquidityContent from "@/components/leverage-liquidity/leverageLiquidityContent";
+import LeverageLiquidityPage from "@/components/leverage-liquidity/leverageLiquidityPage";
+import MintForm from "@/components/leverage-liquidity/mintForm/mintForm";
+import type {
+  TCollateral,
+  TCollateralResp,
+  TVaults,
+  VaultFieldFragment,
+} from "@/lib/types";
+import { readContract } from "@/lib/viemClient";
+import { executeVaultsQuery } from "@/server/queries/vaults";
+import { kv } from "@vercel/kv";
+import { randomInt, createHash } from "crypto";
+import { AssistantContract } from "@/contracts/assistant";
+import { parseUnits } from "viem";
+
+// import { unstable_cache } from "next/cache";
+export const revalidate = 10;
+const getVaults = async () => {
+  const vaults = await executeVaultsQuery();
+  const randomI = randomInt(1000);
+  return { vaults, randomI };
+};
+const getCollateralAmounts = async (vaultIds: number[]) => {
+  const result = await readContract({
+    ...AssistantContract,
+    functionName: "getReserves",
+    args: [[...vaultIds]],
+  });
+  return result;
+};
+export default async function Home() {
+  let vaultQuery;
+  const vaults = await kv.get("vaults");
+  if (!vaults) {
+    const v = await getVaults();
+    vaultQuery = v.vaults;
+    // Don't block with await only need for next cache
+    kv.set("vaults", JSON.stringify(vaultQuery), { ex: 60 * 4 }).catch((e) => {
+      console.log(e, "Failed to store in KV");
+    });
+  } else {
+    vaultQuery = vaults as { vaults: VaultFieldFragment[] };
+  }
+  const vaultIds = vaultQuery?.vaults.map((v) => parseInt(v.vaultId));
+  const vaultIdHash = createHash("md5")
+    .update(JSON.stringify(vaultIds))
+    .digest("hex");
+  let collateral: TCollateral;
+  const resp = await kv.get(vaultIdHash);
+
+  if (resp) {
+    collateral = (resp as TCollateralResp).map((c) => {
+      return {
+        reserveLPers: parseUnits(c.reserveLPers, 0),
+        reserveApes: parseUnits(c.reserveApes, 0),
+        tickPriceX42: parseUnits(c.tickPriceX42, 0),
+      };
+    });
+  } else {
+    collateral = await getCollateralAmounts(vaultIds ?? []);
+  }
+
+  kv.set(
+    vaultIdHash,
+    JSON.stringify(
+      collateral.map((c) => ({
+        ...c,
+        reserveApes: c.reserveApes.toString(),
+        reserveLPers: c.reserveLPers.toString(),
+        tickPriceX42: c.tickPriceX42.toString(),
+      })),
+    ),
+    {
+      ex: 60 * 5,
+    },
+  ).catch((e) => {
+    console.log("Error setting vaultIdHash", e);
+  });
+
+  vaultQuery = {
+    vaults: vaultQuery?.vaults.map((v, i) => ({
+      ...v,
+      apeCollateral: collateral[i]?.reserveApes ?? 0n,
+      teaCollateral: collateral[i]?.reserveLPers ?? 0n,
+    })),
+  } as TVaults;
+  return (
+    <main className="flex  flex-col items-center justify-center text-white">
+      <LeverageLiquidityPage title={"Take on Leverage"}>
+        <LeverageLiquidityContent
+          isApe
+          form={<MintForm isApe vaultsQuery={vaultQuery} />}
+          vaultsQuery={vaultQuery}
+        ></LeverageLiquidityContent>
+      </LeverageLiquidityPage>
+    </main>
+  );
+}
