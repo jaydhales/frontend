5717050bf24c1fadc6feaa29cc3211c30d7f2596
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-10-06 08:54:10 -0700

    fix: Fixed health calculation

--- src/components/leverage-liquidity/vaultTable/hooks/useCalculateVaultHealth.ts
+++ src/components/leverage-liquidity/vaultTable/hooks/useCalculateVaultHealth.ts
@@ -17,18 +17,18 @@ export default function useCalculateVaultHealth({
   isApe,
 }: Props): VariantProps<typeof badgeVariants> {
   const ape = parseFloat(formatUnits(apeCollateral, 18));
-  const gentlement = parseFloat(formatUnits(teaCollateral, 18));
+  const gentlemen = parseFloat(formatUnits(teaCollateral, 18));
   const leverageRatio = mapLeverage(leverageTier.toString());
   const Gmin = (parseFloat(leverageRatio ?? "0") - 1) * ape;
-  const mult = Gmin * 1.2;
-  if (mult > gentlement) {
-    return isApe ? { variant: "red" } : { variant: "green" };
+  const mult = Gmin * 1.25;
+  if (gentlemen > mult) {
+    return isApe ? { variant: "green" } : { variant: "red" };
   }
-  if (Gmin > gentlement) {
+  if (gentlemen > Gmin) {
     return isApe ? { variant: "yellow" } : { variant: "yellow" };
   }
-  if (gentlement > Gmin) {
-    return isApe ? { variant: "green" } : { variant: "red" };
+  if (Gmin > gentlemen) {
+    return isApe ? { variant: "red" } : { variant: "green" };
   }
   return { variant: "green" };
 }
