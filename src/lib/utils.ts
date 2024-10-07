7ae9220c63f25de5dbc9527cc2da7edaee8d4cd7
Tarp <travistarpley0@gmail.com>
2024-10-07 12:48:41 -0700

    feat: Added tokenSchema and getTokenJson util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -34,6 +34,27 @@ export function getLogoAsset(address: `0x${string}` | undefined) {
   const chainName = getChainName();
   return `https://raw.githubusercontent.com/fusionxx23/assets/master/blockchains/${chainName}/assets/${getAddress(address)}/logo.png`;
 }
+
+export function getLogoJson(address: `0x${string}` | undefined) {
+  if (!address) {
+    return "";
+  }
+  const getChainName = () => {
+    const chainId = env.NEXT_PUBLIC_CHAIN_ID;
+    if (chainId === "1") {
+      return "ethereum";
+    }
+    if (chainId === "11155111") {
+      return "sepolia";
+    }
+    if (chainId === "17000") {
+      return "holesky";
+    }
+  };
+
+  const chainName = getChainName();
+  return `https://raw.githubusercontent.com/fusionxx23/assets/master/blockchains/${chainName}/assets/${getAddress(address)}/info.json`;
+}
 export async function getAssetInfo(address: TAddressString | undefined) {
   if (!address) {
     throw Error("No address provided.");
