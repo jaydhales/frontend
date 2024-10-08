6545df6ec58ae4cba6798f176e816b7effdd69ee
Travis Tarpley <34798887+fusionxx23@users.noreply.github.com>
2024-10-07 20:58:23 -0700

    feat: Added toast

---
+++ src/components/ui/warning.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+import { useToast } from "../shared/hooks/useToast";
+import { useEffect } from "react";
+import useGetChainId from "../shared/hooks/useGetChainId";
+import { useChainId } from "wagmi";
+
+export default function Warning() {
+  const { toast } = useToast();
+  const chainId = useGetChainId();
+  const wagmiChainId = useChainId();
+
+  useEffect(() => {
+    if (wagmiChainId === 11155111) {
+      toast({
+        title: "Warning",
+        description:
+          "You are currently on the Sepolia testnet. All transactions use test tokens (including ETH and ERC20), not real money.",
+      });
+    }
+  }, [chainId, wagmiChainId, toast]);
+  return <></>;
+}
