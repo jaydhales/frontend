a7a165f03763fa9f476dcffd5cae639a0b1da9dc
Tarp <travistarpley0@gmail.com>
2024-10-21 15:22:35 -0700

    feat: Added Fee info to Mint Button

--- src/components/leverage-liquidity/mintForm/submit.tsx
+++ src/components/leverage-liquidity/mintForm/submit.tsx
@@ -1,15 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { useConnectModal } from "@rainbow-me/rainbowkit";
-import React from "react";
+import React, { useState } from "react";
 import { useAccount } from "wagmi";
 import { ESubmitType } from "./hooks/useCheckSubmitValid";
+import { ChevronDown } from "lucide-react";
+import { formatNumber } from "@/lib/utils";
 
 const SubmitContext = React.createContext(undefined);
 
 const Root = ({ children }: { children: React.ReactNode }) => {
   return (
     <SubmitContext.Provider value={undefined}>
-      <div className=" flex-col flex items-center justify-center gap-y-2 pt-4">
+      <div className=" flex flex-col items-center justify-center gap-y-2 pt-4">
         {children}
       </div>
     </SubmitContext.Provider>
@@ -20,7 +22,7 @@ const Errors = ({ children }: { children: React.ReactNode }) => {
   const { address } = useAccount();
   return (
     <div className="md:w-[450px]">
-      <p className="h-[20px] text-left text-sm text-red-400">
+      <p className="h-[14px] text-left text-sm text-red-400">
         {/* Don't show form errors if users is not connected. */}
         {address && <>{children}</>}
       </p>
@@ -61,10 +63,67 @@ const OpenTransactionModalButton = ({
     </Button>
   );
 };
+const FeeInfo = ({
+  fee,
+  isApe,
+  feeValue,
+  deposit,
+}: {
+  fee: string | undefined;
+  deposit: string | undefined;
+  feeValue: string | undefined;
+  isApe: boolean;
+}) => {
+  const [open, setOpen] = useState(false);
+  if (fee === undefined || deposit === undefined || feeValue === undefined) {
+    return undefined;
+  }
+  return (
+    <div className="w-[450px] text-gray-200">
+      <div className="flex justify-between text-[14px]">
+        <div className="flex gap-x-1">
+          <span>{fee ? fee.toString() + "%" : "0%"} </span> <span>in fees</span>
+        </div>
+        <div
+          onClick={() => {
+            setOpen(!open);
+          }}
+          className="cursor-pointer"
+        >
+          <ChevronDown
+            data-state={open ? "open" : "close"}
+            className="transition-transform data-[state=open]:rotate-180"
+          />
+        </div>
+      </div>
+
+      {open && (
+        <div className="animate-fade-in pt-1 text-[14px] text-gray-100">
+          <div className="flex justify-between py-1">
+            <span>Fee Amount</span>
+            <span className="text-white">
+              {formatNumber(
+                parseFloat(deposit ?? "0") * (parseFloat(fee ?? "0") / 100),
+              )}
+              <span className="text-gray-400"> {feeValue}</span>
+            </span>
+          </div>
+          <div className="pt-1  text-[14px] text-gray-500">
+            {isApe
+              ? "Apes pay fees only twice: once when minting and once when burning their APE tokens. No additional fees are charged while holding APE tokens, regardless of the duration."
+              : "Gentlemen pay fees when minting and burning liquidity. These fees deter attacks and reward early liquidity providers. It's advantageous to mint TEA early and burn it late."}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+// form.getValues("long").split(",")[1]
 
 const MintFormSubmit = {
   Root,
   Errors,
+  FeeInfo,
   OpenTransactionModalButton,
   ConnectButton,
 };
