2a8cbbeefd66712f15012e6f061e897329273309
Tarp <travistarpley0@gmail.com>
2024-10-21 14:40:40 -0700

    feat: Changed checkbox size

--- src/components/ui/checkbox.tsx
+++ src/components/ui/checkbox.tsx
@@ -13,15 +13,15 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={cn(
-      "peer h-8 w-8 rounded-sm border-2 border-primary ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-accent data-[state=checked]:text-primary-foreground",
-      className
+      "peer h-6 w-6 rounded-sm border-2 border-primary ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-accent data-[state=checked]:text-primary-foreground",
+      className,
     )}
     {...props}
   >
     <CheckboxPrimitive.Indicator
       className={cn("flex items-center justify-center text-current")}
     >
-      <Check className="h-7 w-7" />
+      <Check className="h-5 w-5" />
     </CheckboxPrimitive.Indicator>
   </CheckboxPrimitive.Root>
 ));
