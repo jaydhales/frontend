1751a8fa4e7aa1e982e4885a7b7717d5a9c4fc0c
Tarp <travistarpley0@gmail.com>
2024-10-21 18:45:33 -0700

    fix: Added container styles

--- src/app/portfolio/loading.tsx
+++ src/app/portfolio/loading.tsx
@@ -2,5 +2,9 @@ import PorfolioSkeleton from "@/components/portfolio/porfolioSkeleton";
 import React from "react";
 
 export default function Loading() {
-  return <PorfolioSkeleton />;
+  return (
+    <main className="flex flex-col items-center justify-center text-white">
+      <PorfolioSkeleton />
+    </main>
+  );
 }
