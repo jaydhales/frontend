db7b74cfb51dd2644f72915e95ac7be98edd7aa2
Tarp <travistarpley0@gmail.com>
2024-10-21 18:46:00 -0700

    feat: Added animate containers

--- src/components/portfolio/porfolioSkeleton.tsx
+++ src/components/portfolio/porfolioSkeleton.tsx
@@ -7,23 +7,26 @@ export default function PorfolioSkeleton() {
     <div className="lg:w-[1000px] ">
       <div className="pt-[44px]"></div>
       <Container className="space-y-4">
-        <Card className=" w-full px-4 py-4">
+        <Card className=" flex h-[250px] w-full flex-col justify-between px-4 py-4">
           <div className="space-y-3 ">
+            <TitleSkeleton />
             <div className="grid grid-cols-2 gap-x-3  pt-2"></div>
           </div>
-        </Card>
-        <Card className="py-4">
-          <div className="rounded-md bg-secondary-400 px-4 py-2">
-            <div className="flex  items-center justify-between pb-4 lg:pb-8 ">
-              <h2 className="flex items-center gap-x-1 pb-1 text-sm text-gray-200 ">
-                <span>My Tokens</span>
-              </h2>
-              {/* <Slider value={value} setValue={setValue} /> */}
-            </div>
-            <div className="">{/* <BurnTable filter={value} /> */}</div>
+          <div className="flex gap-x-2">
+            <div className="h-[76px] w-full animate-pulse rounded-md bg-gray-800"></div>
+            <div className="h-[76px] w-full animate-pulse rounded-md bg-gray-800"></div>
           </div>
+        </Card>
+        <Card className="h-[210px] py-4">
+          <div className="h-full w-full animate-pulse rounded-md bg-gray-800"></div>
         </Card>
       </Container>
     </div>
+  );
+}
+
+function TitleSkeleton() {
+  return (
+    <div className="mb-2 h-[16px] w-[30px] animate-pulse rounded-md bg-gray-800/70"></div>
   );
 }
