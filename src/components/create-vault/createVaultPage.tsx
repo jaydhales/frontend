7dbfeb284152a91767b93f02bfe6ae43c07800ba
Tarp <travistarpley0@gmail.com>
2024-10-22 12:08:58 -0700

    feat: increased card size

--- src/components/create-vault/createVaultPage.tsx
+++ src/components/create-vault/createVaultPage.tsx
@@ -8,7 +8,7 @@ export default function CreateVaultPage() {
       {/* <PageHeader> Create Vault</PageHeader> */}
       <div className="flex w-[600px] flex-col items-center">
         <br />
-        <Card className="md:w-[600px]">
+        <Card className="md:w-[650px]">
           <CreateVaultForm />
         </Card>
       </div>
