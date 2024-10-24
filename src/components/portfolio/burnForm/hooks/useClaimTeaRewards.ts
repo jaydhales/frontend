751fd78dcf5a16ca5db1ff5c45030b6d9884bbf2
Tarp <travistarpley0@gmail.com>
2024-10-23 17:50:43 -0700

    logs

--- src/components/portfolio/burnForm/hooks/useClaimTeaRewards.ts
+++ src/components/portfolio/burnForm/hooks/useClaimTeaRewards.ts
@@ -8,6 +8,12 @@ export function useClaimTeaRewards({
   vaultId: bigint;
   claimAndStake: boolean;
 }) {
+  console.log(claimAndStake, "claimAndStake");
+  console.log({
+    ...SirContract,
+    functionName: claimAndStake ? "lPerMintAndStake" : "lPerMint",
+    args: [vaultId],
+  });
   const { data: teaRewardData } = useSimulateContract({
     ...SirContract,
     functionName: claimAndStake ? "lPerMintAndStake" : "lPerMint",
