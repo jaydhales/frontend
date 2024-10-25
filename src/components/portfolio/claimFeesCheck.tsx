715c9a0653a5c32e3dca5c47d315b6cf6a251924
Tarp <travistarpley0@gmail.com>
2024-10-24 18:24:39 -0700

    feat: changed checkbox and dividends amt styles

--- src/components/portfolio/claimFeesCheck.tsx
+++ src/components/portfolio/claimFeesCheck.tsx
@@ -1,6 +1,7 @@
 import { FormLabel } from "@/components/ui/form";
 
 import { Checkbox } from "@/components/ui/checkbox";
+import { formatNumber } from "@/lib/utils";
 
 interface Props {
   dividends?: string;
@@ -14,20 +15,21 @@ const ClaimFeesCheckbox = ({ dividends, value, onChange }: Props) => {
     return;
   }
   return (
-    <div className="flex items-center justify-between pt-2">
-      <div className="flex items-center gap-x-4">
+    <div className=" justify-between gap-x-4 pt-2">
+      <div className="flex justify-end gap-x-1 pb-2">
+        <span className="justify-end text-sm text-[#B6B6C9]">
+          Dividends: {""}
+          {formatNumber(dividends, 6)} ETH
+        </span>
+      </div>
+      <div className="flex items-center justify-end gap-x-4 pb-2">
         <Checkbox
           checked={value}
           onCheckedChange={(value) => {
             onChange(Boolean(value)); // Call onChange to update the state in UnstakeForm
           }}
         ></Checkbox>
-        <FormLabel className="pt-2 text-lg font-medium">
-          Claim my fees
-        </FormLabel>
-      </div>
-      <div className="text-sm text-[#B6B6C9]">
-        Amount to claim: {dividends} ETH
+        <FormLabel className=" text-md font-medium">Claim Dividends</FormLabel>
       </div>
     </div>
   );
