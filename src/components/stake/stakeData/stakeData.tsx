06a424ce1acaf6cab06caa902494cb37daadf1d5
Tarp <travistarpley0@gmail.com>
2024-10-26 16:17:38 -0700

    feat: Added Your Staked SIR

--- src/components/stake/stakeData/stakeData.tsx
+++ src/components/stake/stakeData/stakeData.tsx
@@ -1,9 +1,10 @@
 "use client";
 
 import { api } from "@/trpc/react";
-import { useEffect, useMemo } from "react";
+import { useMemo } from "react";
 import { formatUnits } from "viem";
 import ToolTip from "@/components/ui/tooltip";
+import { useGetStakedSir } from "@/components/shared/hooks/useGetStakedSir";
 
 interface supplyProps {
   data?: bigint;
@@ -20,19 +21,13 @@ const StakeData = () => {
       return totalSupply - unstakedSupply;
     }
   }, [unstakedSupply, totalSupply]);
-
-  useEffect(() => {
-    console.log("---DATA---");
-    console.log(`Unstaked SIR Supply: ${unstakedSupply}`);
-    console.log(`Total SIR Supply: ${totalSupply}`);
-    console.log(`Total Value Locked: ${totalValueLocked}`);
-  }, [unstakedSupply, totalSupply, totalValueLocked]);
+  const userStakedSir = useGetStakedSir();
 
   return (
-    <div className="mx-auto grid w-[600px] grid-cols-2 gap-x-4 py-[24px] ">
+    <div className="mx-auto grid w-[600px] grid-cols-3 gap-x-4 py-[24px] ">
       <div className="flex flex-col  items-center justify-center gap-2 rounded-md bg-secondary py-2">
         <div className="text-sm font-medium text-gray-300">
-          Total SIR Staked
+          Total Staked SIR
         </div>
         {/* <div className="text-2xl font-semibold font-lora">
           {parseFloat(formatUnits(totalValueLocked ?? 0n, 12)).toFixed(4)}
@@ -50,6 +45,16 @@ const StakeData = () => {
               return value.toFixed(2);
             }
           })()}
+        </div>
+      </div>
+
+      <div className="flex flex-col items-center justify-center gap-2 rounded-md bg-secondary py-2">
+        <div className="flex w-full flex-row items-center justify-center">
+          <div className="px-2 text-sm text-gray-300">Your Staked SIR</div>
+          {/* <ToolTip>Tool tip info.</ToolTip> */}
+        </div>
+        <div className="font-lora text-2xl ">
+          {formatUnits(userStakedSir, 12)}
         </div>
       </div>
       <div className="flex flex-col items-center justify-center gap-2 rounded-md bg-secondary py-2">
