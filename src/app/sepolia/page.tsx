623f6e718417c1ebfea2a9617c91c8eb3b262fb1
Tarp <travistarpley0@gmail.com>
2024-10-26 14:21:20 -0700

    feat: Added sepolia utilities page

--- src/app/sepolia/page.tsx
+++ src/app/sepolia/page.tsx
@@ -1,77 +1,95 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Container } from "@/components/ui/container";
+import { MethContract } from "@/contracts/meth";
 import React from "react";
+import { useSimulateContract, useWriteContract } from "wagmi";
 
 export default function Page() {
-  return (
-    <Container>
-      <Card className="flex min-h-screen flex-col items-center justify-center  p-4">
-        <h1 className="mb-6 text-3xl font-bold text-white">
-          Sepolia Testnet Faucet e & Token Interaction
-        </h1>
+  const { data: MethData } = useSimulateContract({
+    ...MethContract,
+    functionName: "mint",
+  });
+  const { data: TarpData } = useSimulateContract({
+    ...MethContract,
+    functionName: "mint",
+  });
+  const { writeContract } = useWriteContract();
+  const onSubmitMeth = () => {
+    if (MethData) {
+      writeContract(MethData?.request);
+    }
+  };
 
-        <div className="space-y-4">
+  console.log(TarpData, MethData);
+  const onSubmitTarp = () => {
+    if (TarpData) {
+      writeContract(TarpData?.request);
+    }
+  };
+  return (
+    <Container className="pt-[44px]">
+      <Card className="flex flex-col items-center justify-center p-4 text-white">
+        <h1 className="mb-6 text-2xl  ">Sepolia Utilities</h1>
+
+        <div className="flex gap-x-3 text-blue-300">
           <a
             href="https://www.infura.io/faucet/sepolia"
             target="_blank"
-            className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            className="rounded px-4 py-2"
           >
             Mint ETH
           </a>
           <a
             href="https://faucets.chain.link/sepolia"
             target="_blank"
-            className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            className="rounded  px-4 py-2"
           >
             Mint LINK
           </a>
           <a
             href="https://faucet.circle.com/"
             target="_blank"
-            className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            className="rounded  px-4 py-2"
           >
             Mint USDC
           </a>
         </div>
 
-        <div className="mt-8 w-full max-w-md rounded-lg bg-white p-6 shadow-md">
-          <h2 className="mb-4 text-xl font-bold">Token Details</h2>
+        <div className="mt-8 w-full max-w-md rounded-lg bg-secondary-400 bg-opacity-40 p-6 shadow-md ">
+          <h2 className="mb-4 text-xl text-gray-300">Mock Tokens</h2>
+          <div className="flex gap-x-4">
+            <div className="">
+              <Button
+                disabled={!MethData}
+                onClick={() => {
+                  onSubmitMeth();
+                }}
+                className="mt-2 rounded  px-4 py-2  "
+              >
+                Mint 1000 METH
+              </Button>
+            </div>
 
-          <div className="mb-4">
-            <p>
-              <strong>Name:</strong> Token A
-            </p>
-            <p>
-              <strong>Symbol:</strong> TKA
-            </p>
-            <p>
-              <strong>Address:</strong> 0x123...abc
-            </p>
-            <button className="mt-2 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700">
-              Mint 1000 TKA
-            </button>
-          </div>
-
-          <div className="mb-4">
-            <p>
-              <strong>Name:</strong> Token B
-            </p>
-            <p>
-              <strong>Symbol:</strong> TKB
-            </p>
-            <p>
-              <strong>Address:</strong> 0x456...def
-            </p>
-            <button className="mt-2 rounded bg-green-500 px-4 py-2 font-bold text-white hover:bg-green-700">
-              Mint 1000 TKB
-            </button>
+            <div className="mb-4">
+              <Button
+                disabled={!TarpData}
+                onClick={() => {
+                  onSubmitTarp();
+                }}
+                className="mt-2 rounded  px-4 py-2  "
+              >
+                Mint 1000 TARP
+              </Button>
+            </div>
           </div>
         </div>
 
         <a
           href="https://interface-jet.vercel.app/#/swap?chain=sepolia"
           target="_blank"
-          className="mt-8 rounded bg-purple-500 px-4 py-2 font-bold text-white hover:bg-purple-700"
+          className="mt-8 rounded bg-gold px-4 py-2 text-black  hover:bg-gold/95"
         >
           Trade on Sepolia Uniswap
         </a>
