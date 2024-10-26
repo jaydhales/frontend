003425da35a5016f2b383c118f953cf2e1409b4f
Tarp <travistarpley0@gmail.com>
2024-10-25 17:38:11 -0700

    feat: Added bg

--- src/components/portfolio/sirCard.tsx
+++ src/components/portfolio/sirCard.tsx
@@ -1,163 +1,36 @@
-import { api } from "@/trpc/react";
-import {
-  useAccount,
-  useSimulateContract,
-  useWaitForTransactionReceipt,
-  useWriteContract,
-} from "wagmi";
-import { useEffect } from "react";
-import { CircleCheck } from "lucide-react";
-import { SirContract } from "@/contracts/sir";
-import type { StaticImageData } from "next/image";
-import { useState } from "react";
-import Image from "next/image";
 import { formatUnits } from "viem";
 import { formatNumber } from "@/lib/utils";
-import { Button } from "../ui/button";
-import sirIcon from "../../../public/images/sir-logo.svg";
-import TransactionModal from "../shared/transactionModal";
-import { TransactionStatus } from "../leverage-liquidity/mintForm/transactionStatus";
-import { StakeModal } from "../shared/stake/stakeModal";
-import StakeFormProvider from "../providers/stakeFormProvider";
+import { api } from "@/trpc/react";
+import { useAccount } from "wagmi";
+import StakeCard from "./stakeCard";
 
 export function SirCard() {
   const { isConnected, address } = useAccount();
-  const { data: totalBalance } = api.user.getTotalSirBalance.useQuery(
+  const { data: totalBalance } = api.user.getUnstakedSirBalance.useQuery(
     {
       user: address,
     },
     { enabled: isConnected },
   );
-  const { data: unclaimedData } =
-    api.user.getUnclaimedContributorRewards.useQuery(
-      { user: address },
-      { enabled: isConnected },
-    );
-  const [open, setOpen] = useState(false);
-  const { data } = useSimulateContract({
-    ...SirContract,
-    functionName: "contributorMint",
-  });
-  const { writeContract, reset, isPending, data: hash } = useWriteContract();
-  console.log(hash, "HASH");
-  const { isLoading: isConfirming, isSuccess: isConfirmed } =
-    useWaitForTransactionReceipt({
-      hash,
-    });
-  const onSubmit = () => {
-    if (isConfirmed) {
-      setOpen(false);
-      return;
-    }
-    if (data?.request) {
-      writeContract(data?.request);
-    }
-  };
-  const utils = api.useUtils();
-  useEffect(() => {
-    if (isConfirmed && !open) {
-      utils.user.getUnclaimedContributorRewards
-        .invalidate()
-        .catch((e) => console.log(e));
-      reset();
-    }
-  }, [isConfirmed, reset, open, utils.user.getUnclaimedContributorRewards]);
-  const unclaimedRewards = unclaimedData ?? 0n;
-  const [stakeModal, setStakeModal] = useState(false);
   return (
-    <div className=" border-b border-secondary-200 pb-2">
-      <div className="flex justify-between rounded-md px-2 pb-2 text-2xl">
+    <div className="rounded-md bg-secondary-600 bg-opacity-40 p-2 pb-2">
+      <div className=" flex justify-between rounded-md text-2xl">
         <div className="flex gap-x-2 ">
-          <div>
-            <h2 className="text-sm text-gray-200">Total SIR</h2>
-            <div className="flex justify-between text-3xl   ">
-              <div className="flex items-end gap-x-1">
-                <span>{formatNumber(formatUnits(totalBalance ?? 0n, 12))}</span>
-                <h2 className="text-sm font-light text-gray-400">SIR</h2>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <TransactionModal.Root setOpen={setOpen} open={open}>
-          <TransactionModal.Close setOpen={setOpen} />
-          <TransactionModal.InfoContainer>
-            <TransactionStatus
-              action="Claim"
-              waitForSign={isPending}
-              showLoading={isConfirming}
-              isConfirmed={isConfirmed}
-            />
-            {!isConfirmed && (
-              <div className="space-x-0.5 py-2">
-                <span className="text-lg">
-                  {formatNumber(formatUnits(unclaimedData ?? 0n, 12), 8)}
-                </span>
-                <span className="text-gray-400">SIR</span>
-              </div>
-            )}
-            {isConfirmed && (
-              <div>
-                <div className="flex justify-center">
-                  <CircleCheck size={40} color="#F0C775" />
-                </div>
-                <h2 className="text-center">Transaction Successful!</h2>
-              </div>
-            )}
-          </TransactionModal.InfoContainer>
-
-          <TransactionModal.StatSubmitContainer>
-            <TransactionModal.SubmitButton
-              isConfirmed={isConfirmed}
-              loading={isPending || isConfirming}
-              disabled={isPending || isConfirming}
-              onClick={() => onSubmit()}
-            >
-              Claim
-            </TransactionModal.SubmitButton>
-          </TransactionModal.StatSubmitContainer>
-        </TransactionModal.Root>
-
-        <div className="items-center justify-between ">
-          <div className="flex justify-end">
-            <Button
-              onClick={() => setStakeModal(true)}
-              type="button"
-              className="p-2"
-            >
-              Stake
-            </Button>
-            <StakeFormProvider>
-              <StakeModal
-                setOpen={setStakeModal}
-                open={stakeModal}
-              ></StakeModal>
-            </StakeFormProvider>
-          </div>
-          {data?.request && unclaimedRewards > 0n && (
+          <div className="flex w-full justify-between">
             <div>
-              <h4 className="px-4 text-center text-[14px] text-gray-300">
-                Claim Contributor Rewards
-              </h4>
-              <Button
-                onClick={() => setOpen(true)}
-                className="w-full space-x-1 bg-gold px-4 py-2 font-bold text-black hover:bg-gold/90"
-              >
-                <span>Claim </span>
-                <span className="">
-                  {formatNumber(formatUnits(unclaimedData ?? 0n, 12), 6)}
-                </span>
-
-                <Image
-                  height={18}
-                  width={18}
-                  src={sirIcon as StaticImageData}
-                  alt="Sir Icon"
-                />
-              </Button>
+              <h2 className="pb-1 text-sm text-gray-200">Unstaked SIR</h2>
+              <div className="flex justify-between text-3xl   ">
+                <div className="flex items-end gap-x-1">
+                  <span>
+                    {formatNumber(formatUnits(totalBalance ?? 0n, 12))}
+                  </span>
+                  <h2 className="text-sm font-light text-gray-400">SIR</h2>
+                </div>
+              </div>
             </div>
-          )}
+          </div>
         </div>
+        <StakeCard />
       </div>
     </div>
   );
