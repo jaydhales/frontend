7ed9eb21449cef375b9370158204ddeaec9a5b70
Tarp <travistarpley0@gmail.com>
2024-10-31 13:16:30 -0700

    refactor: moved footer component file

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,7 +1,10 @@
+import FeedBackForm from "./feedbackForm";
+
 export default function Footer() {
   return (
     <div className="flex  flex-grow items-end px-[16px] py-[24px]">
       <div>
+        <FeedBackForm />
         <nav className="flex gap-x-4 text-[14px] text-muted-foreground ">
           <a className="hover:text-white" href="">
             About
