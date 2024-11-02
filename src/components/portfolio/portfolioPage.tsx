97855c19a7559d24b29c857b68c89cdc512accd0
Tarp <travistarpley0@gmail.com>
2024-11-02 12:48:17 -0700

    feat: adding responsive grid cols

--- src/components/portfolio/portfolioPage.tsx
+++ src/components/portfolio/portfolioPage.tsx
@@ -13,10 +13,10 @@ export default function PortfolioPage() {
   const [value, setValue] = useState<"ape" | "tea" | "all">("all");
   return (
     <div className="lg:w-[900px] ">
-      <div className="pt-[44px]" />
-      <Explainer page={EPage.PORTFOLIO} />
+      {/* <div className="pt-[44px]" /> */}
       <Container className="space-y-4">
-        <div className="grid grid-cols-2 gap-x-4">
+        <Explainer page={EPage.PORTFOLIO} />
+        <div className="grid gap-4 md:grid-cols-2">
           <Card>
             <div className="flex h-full flex-col justify-between">
               <div className="flex justify-between">
