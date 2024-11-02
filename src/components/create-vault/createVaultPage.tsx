13619a89fa34e1c9fc5ff69188e8a3065a1210bf
Tarp <travistarpley0@gmail.com>
2024-11-02 12:49:38 -0700

    feat: Added container and width breakpoint

--- src/components/create-vault/createVaultPage.tsx
+++ src/components/create-vault/createVaultPage.tsx
@@ -2,18 +2,19 @@ import { EPage } from "@/lib/types";
 import Explainer from "../shared/explainer";
 import { Card } from "../ui/card";
 import CreateVaultForm from "./createVaultForm";
+import { Container } from "../ui/container";
 
 export default function CreateVaultPage() {
   return (
     <div>
       {/* <PageHeader> Create Vault</PageHeader> */}
-      <div className="flex w-[650px] flex-col items-center">
         <div className="pt-[44px]" />
+      <Container className="flex max-w-[650px] flex-col items-center md:w-[650px]">
         <Explainer page={EPage.CREATE_VAULT} />
         <Card className="md:w-[650px]">
           <CreateVaultForm />
         </Card>
-      </div>
+      </Container>
     </div>
   );
 }
