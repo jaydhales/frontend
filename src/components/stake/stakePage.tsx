225144304478e994498619e3e1b4f308969f77c6
Tarp <travistarpley0@gmail.com>
2024-11-02 12:47:34 -0700

    feat: Adding width breakpoint

--- src/components/stake/stakePage.tsx
+++ src/components/stake/stakePage.tsx
@@ -3,11 +3,12 @@ import StakeData from "@/components/stake/stakeData/stakeData";
 import Explainer from "../shared/explainer";
 import { EPage } from "@/lib/types";
 import { Container } from "../ui/container";
+import PageHeadingSpace from "../shared/pageHeadingSpace";
 
 const stakePage = ({ children }: { children: ReactNode }) => {
   return (
     <div className="">
-      <Container className="w-[600px] pt-[44px]">
+      <Container className="max-w-[600px]  md:w-[600px]">
         <Explainer page={EPage.STAKE} />
         <StakeData />
         {children}
