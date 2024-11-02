c4469928c3cc3823c95ae88d8e2ac3c5b5b5b7c2
Tarp <travistarpley0@gmail.com>
2024-11-02 12:46:46 -0700

    formatting

--- src/components/stake/stakeTabs.tsx
+++ src/components/stake/stakeTabs.tsx
@@ -7,16 +7,14 @@ import StakeCard from "./stakeCard";
 const StakeTabs = () => {
   return (
     <div className="pt-6">
-      <Container>
         <Card className="mx-auto grid w-[600px]  gap-y-4">
-          <div>
-            <StakeCard />
-          </div>
-          <div>
-            <ClaimCard />
-          </div>
-        </Card>
-      </Container>
+        <div>
+          <StakeCard />
+        </div>
+        <div>
+          <ClaimCard />
+        </div>
+      </Card>
     </div>
   );
 };
