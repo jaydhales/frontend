08dc5ce200254c240e8721ccf8d8cd9e8ad7899f
Tarp <travistarpley0@gmail.com>
2024-11-06 12:45:52 -0800

    refactor: moved get asset functions to seperate file

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -2,13 +2,8 @@ import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { formatUnits, getAddress } from "viem";
 import type { TAddressString, TMintFormFields, TVaults } from "../types";
-import { assetSchema } from "../schemas";
 import { z } from "zod";
 import numeral from "numeral";
-import { ASSET_REPO } from "@/data/constants";
-import { env } from "@/env";
-import sirIcon from "../../public/images/sir-logo.svg";
-import type { StaticImageData } from "next/image";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -16,58 +11,6 @@ export function add(n: number, a: number) {
   return n + a;
 }
 
-export function getLogoAsset(address: `0x${string}` | undefined) {
-  if (!address) {
-    return "";
-  }
-  const getChainName = () => {
-    const chainId = env.NEXT_PUBLIC_CHAIN_ID;
-    if (chainId === "1") {
-      return "ethereum";
-    }
-    if (chainId === "11155111") {
-      return "sepolia";
-    }
-    if (chainId === "17000") {
-      return "holesky";
-    }
-  };
-  if (address === env.NEXT_PUBLIC_SIR_ADDRESS) {
-    return sirIcon as StaticImageData;
-  }
-  const chainName = getChainName();
-  return `${ASSET_REPO}/blockchains/${chainName}/assets/${getAddress(address)}/logo.png`;
-}
-
-export function getLogoJson(address: `0x${string}` | undefined) {
-  if (!address) {
-    return "";
-  }
-  const getChainName = () => {
-    const chainId = env.NEXT_PUBLIC_CHAIN_ID;
-    if (chainId === "1") {
-      return "ethereum";
-    }
-    if (chainId === "11155111") {
-      return "sepolia";
-    }
-    if (chainId === "17000") {
-      return "holesky";
-    }
-  };
-
-  const chainName = getChainName();
-  return `${ASSET_REPO}/blockchains/${chainName}/assets/${getAddress(address)}/info.json`;
-}
-export async function getAssetInfo(address: TAddressString | undefined) {
-  if (!address) {
-    throw Error("No address provided.");
-  }
-  const result: unknown = await fetch(
-    `${ASSET_REPO}/blockchains/ethereum/assets/${getAddress(address)}/info.json`,
-  ).then((r) => r.json());
-  return assetSchema.safeParse(result);
-}
 export function mapLeverage(key: string): string | undefined {
   if (key === "2") {
     return "5";
