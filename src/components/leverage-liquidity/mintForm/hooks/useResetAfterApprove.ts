e975a97cf8397c05d9df703b5a8c6496403c788f
Tarp <travistarpley0@gmail.com>
2024-11-10 15:34:17 -0800

    refactor: Added hook and component for mintForm

---
+++ src/components/leverage-liquidity/mintForm/hooks/useResetAfterApprove.ts
@@ -0,0 +1,35 @@
+import { api } from "@/trpc/react";
+import { useEffect, useState } from "react";
+import { ESubmitType } from "./useCheckSubmitValid";
+interface Props {
+  isConfirmed: boolean;
+  reset: () => void;
+  submitType: ESubmitType;
+}
+export function useResetAfterApprove({
+  isConfirmed,
+  reset,
+  submitType,
+}: Props) {
+  const [isApproving, setIsApproving] = useState(false);
+
+  useEffect(() => {
+    if (submitType === ESubmitType.approve) {
+      setIsApproving(true);
+    }
+  }, [submitType]);
+
+  const utils = api.useUtils();
+  useEffect(() => {
+    if (isConfirmed && isApproving) {
+      utils.user.getBalance
+        .invalidate()
+        .then(() => {
+          reset();
+          setIsApproving(false);
+        })
+        .catch((e) => console.log(e));
+    }
+  }, [isApproving, reset, isConfirmed, utils.user.getBalance, setIsApproving]);
+  return isApproving;
+}
---
+++ src/components/leverage-liquidity/mintForm/transactionInfo.tsx
@@ -0,0 +1,87 @@
+import TransactionModal from "@/components/shared/transactionModal";
+import { ESubmitType } from "./hooks/useCheckSubmitValid";
+import { TransactionEstimates } from "./transactionEstimates";
+import { TransactionStatus } from "./transactionStatus";
+import { CircleCheck } from "lucide-react";
+import { formatNumber } from "@/lib/utils";
+import { formatUnits } from "viem";
+
+interface Props {
+  isConfirmed: boolean;
+  decimals: number;
+  isApproving: boolean;
+  isConfirming: boolean;
+  userBalanceFetching: boolean;
+  isPending: boolean;
+  submitType: ESubmitType;
+  tokenReceived: bigint | undefined;
+  isApe: boolean;
+  useEth: boolean;
+  quoteData: bigint | undefined;
+}
+
+export default function TransactionInfo({
+  isApe,
+  quoteData,
+  isConfirming,
+  isConfirmed,
+  isPending,
+  isApproving,
+  submitType,
+  tokenReceived,
+  decimals,
+  useEth,
+  userBalanceFetching,
+}: Props) {
+  if (!isConfirmed) {
+    return (
+      <>
+        <TransactionStatus
+          showLoading={isConfirming || userBalanceFetching}
+          waitForSign={isPending}
+          action={submitType === ESubmitType.mint ? "Mint" : "Approve"}
+        />
+        {submitType === ESubmitType.mint && (
+          <TransactionEstimates
+            isApe={isApe}
+            usingEth={useEth}
+            collateralEstimate={quoteData}
+          />
+        )}
+        {submitType === ESubmitType.mint && (
+          <TransactionModal.Disclaimer>
+            Output is estimated.
+          </TransactionModal.Disclaimer>
+        )}
+        {submitType === ESubmitType.approve && (
+          <TransactionModal.Disclaimer>
+            Approve SIR to send token funds .....
+          </TransactionModal.Disclaimer>
+        )}
+      </>
+    );
+  }
+  if (isConfirming && isApproving) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  if (isConfirmed && !isApproving) {
+    <div className="space-y-2">
+      <div className="flex animate-fade-in justify-center">
+        <CircleCheck size={40} color="#F0C775" />
+      </div>
+      <h2 className="text-center text-gray-300">Transaction Successful!</h2>
+      {Boolean(tokenReceived) && (
+        <h3 className="flex items-center justify-center gap-x-1 ">
+          <span className="text-xl font-bold ">
+            {isApe ? "APE" : "TEA"}{" "}
+            {formatNumber(formatUnits(tokenReceived ?? 0n, decimals), 4)}
+          </span>
+        </h3>
+      )}
+    </div>;
+  }
+}
