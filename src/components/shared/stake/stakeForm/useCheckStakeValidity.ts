69a5a709ef2aa59017de233c9521789992d47ba9
Tarp <travistarpley0@gmail.com>
2024-11-10 15:48:39 -0800

    fix: Fixed reusing mintForm error checker on stake form

---
+++ src/components/shared/stake/stakeForm/useCheckStakeValidity.ts
@@ -0,0 +1,154 @@
+import useGetChainId from "@/components/shared/hooks/useGetChainId";
+import { env } from "@/env";
+import { useMemo } from "react";
+import type { SimulateContractReturnType } from "viem";
+import { parseUnits } from "viem";
+
+interface Props {
+  deposit: string | undefined;
+  depositToken: string;
+  requests: {
+    mintRequest?: SimulateContractReturnType["request"] | undefined;
+    approveWriteRequest?: SimulateContractReturnType["request"] | undefined;
+  };
+  tokenAllowance?: bigint | undefined;
+  tokenBalance: bigint | undefined;
+  ethBalance?: bigint | undefined;
+  mintFetching: boolean;
+  approveFetching?: boolean;
+  useEth?: boolean;
+  decimals: number;
+}
+export enum ESubmitType {
+  "mint",
+  "approve",
+}
+
+/**
+ * Checks if user can submit form.
+ * @returns
+ * isValid -
+ * errorMessage -
+ * submitType - 'approve' | 'mint'
+ */
+export const useCheckStakeValidity = ({
+  deposit,
+  tokenAllowance,
+  mintFetching,
+  requests,
+  approveFetching,
+  tokenBalance,
+  ethBalance,
+  useEth,
+  decimals,
+}: Props) => {
+  const chainId = useGetChainId();
+  const { isValid, errorMessage, submitType } = useMemo(() => {
+    if (chainId?.toString() !== env.NEXT_PUBLIC_CHAIN_ID && Boolean(chainId)) {
+      return {
+        isValid: false,
+        errorMessage: "Wrong network!",
+        submitType: ESubmitType.mint,
+      };
+    }
+
+    if (parseUnits(deposit ?? "0", decimals) <= 0n) {
+      return {
+        isValid: false,
+        errorMessage: "Enter amount greater than 0.",
+        submitType: ESubmitType.mint,
+      };
+    }
+
+    if (useEth) {
+      if ((ethBalance ?? 0n) < parseUnits(deposit ?? "0", decimals)) {
+        return {
+          isValid: false,
+          errorMessage: "Insufficient Balance.",
+          submitType: ESubmitType.mint,
+        };
+      }
+      if (requests.mintRequest) {
+        return {
+          isValid: true,
+          errorMessage: "",
+          submitType: ESubmitType.mint,
+        };
+      }
+      return {
+        isValid: false,
+        errorMessage: "",
+        submitType: ESubmitType.mint,
+      };
+    }
+
+    if ((tokenBalance ?? 0n) < parseUnits(deposit ?? "0", decimals)) {
+      return {
+        isValid: false,
+        errorMessage: "Insufficient Balance.",
+        submitType: ESubmitType.mint,
+      };
+    }
+    // CHECK ALLOWANCE FIRST
+    if (
+      parseUnits(deposit ?? "0", decimals) > (tokenAllowance ?? 0n) &&
+      requests.approveWriteRequest
+    ) {
+      if (requests.approveWriteRequest)
+        return {
+          isValid: true,
+          errorMessage: null,
+          submitType: ESubmitType.approve,
+        };
+      else {
+        if (approveFetching) {
+          return {
+            isValid: false,
+            errorMessage: "",
+            submitType: ESubmitType.approve,
+          };
+        } else {
+          return {
+            isValid: false,
+            errorMessage: "An Approve Error Occured.",
+            submitType: ESubmitType.approve,
+          };
+        }
+      }
+    }
+    if (requests.mintRequest)
+      return {
+        isValid: true,
+        errorMessage: null,
+        submitType: ESubmitType.mint,
+      };
+    else {
+      if (mintFetching) {
+        return {
+          isValid: false,
+          errorMessage: "",
+          submitType: ESubmitType.mint,
+        };
+      } else {
+        return {
+          isValid: false,
+          errorMessage: "Unexpected mint error.",
+          submitType: ESubmitType.mint,
+        };
+      }
+    }
+  }, [
+    chainId,
+    deposit,
+    decimals,
+    useEth,
+    tokenBalance,
+    tokenAllowance,
+    requests.approveWriteRequest,
+    requests.mintRequest,
+    ethBalance,
+    approveFetching,
+    mintFetching,
+  ]);
+  return { isValid, errorMessage, submitType };
+};
