90d0387725e1fb6ce94daf412847ac350fb7035d
Tarp <travistarpley0@gmail.com>
2024-11-11 14:54:48 -0800

    feat: Added SIR USD PRICE constant

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -7,7 +7,7 @@ export const WETH_ADDRESS = (
     ? "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2".toLowerCase()
     : "0xfff9976782d46cc05630d1f6ebab18b2324d6b14".toLowerCase()
 ) as TAddressString;
-
+export const SIR_USD_PRICE = "0.00008271298";
 export const ASSET_REPO =
   "https://raw.githubusercontent.com/SIR-trading/assets/master";
 export const BASE_FEE = parseFloat(env.NEXT_PUBLIC_BASE_FEE);
