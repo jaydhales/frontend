d31b0d2d8bb3e629eec5945d2bc899e082fac3f9
Tarp <travistarpley0@gmail.com>
2024-11-11 13:00:09 -0800

    format

--- src/app/stake/page.tsx
+++ src/app/stake/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import StakePage from "@/components/stake/stakePage";
-
 const StakeHome = () => {
   return (
     <main className="flex  flex-col items-center justify-center text-white">
