bcf85561ef30cebe53938107829057076e156d44
Tarp <travistarpley0@gmail.com>
2024-11-13 12:04:13 -0800

    fix: fixed vitest env

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-
+import { config } from "dotenv";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
   test: {
     environment: "jsdom",
+    env: {
+      ...config({ path: "./.env" }).parsed,
+    },
   },
 });
