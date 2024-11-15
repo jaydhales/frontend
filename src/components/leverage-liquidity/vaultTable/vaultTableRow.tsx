043451ebb4c539246372cdb17942870324e9d1f3
Tarp <travistarpley0@gmail.com>
2024-11-15 10:53:09 -0800

    feat: increase vaultRow badge text size

--- src/components/leverage-liquidity/vaultTable/vaultTableRow.tsx
+++ src/components/leverage-liquidity/vaultTable/vaultTableRow.tsx
@@ -135,7 +135,7 @@ export function VaultTableRow({
         <HoverCard openDelay={0} closeDelay={20}>
           <HoverCardTrigger asChild>
             <div>
-              <Badge {...variant} className="text-nowrap text-[9px]">
+              <Badge {...variant} className="text-nowrap text-[11px]">
                 {`${getLeverageRatio(pool.leverageTier)}x${showPercent() ? " (" + formatNumber(tvlPercent, 2) + "x)" : ""}`}
               </Badge>
             </div>
