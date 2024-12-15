ea20369f8d20064821e9b19be97fa3ebfda68a30
Tarp <travistarpley0@gmail.com>
2024-12-15 12:45:37 -0800

    fix: removed corrupted images

--- src/lib/db/schema.ts
+++
@@ -1,7 +0,0 @@
-import { sql } from "drizzle-orm";
-import { text, sqliteTable, integer } from "drizzle-orm/sqlite-core";
-
-export const feedbackTable = sqliteTable("feedback", {
-  id: integer("id").primaryKey(),
-  feedback: text("feedback").notNull(),
-});
