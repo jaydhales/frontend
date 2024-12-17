5b5279ad605235c437d141db3c5799f50f1854a1
Tarp <travistarpley0@gmail.com>
2024-12-17 13:04:49 -0800

    feat: deprecate old filter logic

--- src/components/leverage-liquidity/mintForm/hooks/useFilterVaults.ts
+++ src/components/leverage-liquidity/mintForm/hooks/useFilterVaults.ts
@@ -16,29 +16,14 @@ export function useFilterVaults({ formData, vaultsQuery }: Props) {
     filterCollateralToken: formData.long.split(",")[0],
     filterLeverage: formData.leverageTier,
   });
+  // have a second query for address searches
 
-  console.log(data, "GET VAUltS DATA");
+  // turn into a set
+  // all unique values
+
   const { versus, leverageTiers, long } = useMemo(() => {
     if (vaultsQuery?.vaults === undefined)
       return { versus: [], leverageTiers: [], long: [] };
-    const matchingPools = vaultsQuery?.vaults.filter((p) => {
-      if (formData.leverageTier) {
-        if (p.leverageTier !== parseInt(formData.leverageTier)) {
-          return false;
-        }
-      }
-      if (formData.long) {
-        if (formData.long.split(",")[0] !== p.collateralToken) {
-          return false;
-        }
-      }
-      if (formData.versus) {
-        if (formData.versus.split(",")[0] !== p.debtToken) {
-          return false;
-        }
-      }
-      return true;
-    });
     const matchingFetchPools = data?.vaults;
     const long = [
       ...new Map(
@@ -54,119 +39,27 @@ export function useFilterVaults({ formData, vaultsQuery }: Props) {
       ...new Set(matchingFetchPools?.map((p) => p.leverageTier)),
     ];
     return { leverageTiers, long, versus };
-  }, [
-    data?.vaults,
-    formData.leverageTier,
-    formData.long,
-    formData.versus,
-    vaultsQuery?.vaults,
-  ]);
+  }, [data?.vaults, vaultsQuery?.vaults]);
   return { versus, leverageTiers, long };
 }
 
-// enum DropsList {
-//   "leverageTier" = 1,
-//   "versus" = 2,
-//   "long" = 3,
-// }
-
-// const [matchingPools, setMatchingPools] = useState<TPool[]>(mockPools);
-// const [firstSelected, setfirstSelected] = useState<DropsList | undefined>();
-// const [secondSelected, setSecondSelected] = useState<DropsList | undefined>();
-// useEffect(() => {
-//   // TODO
-//   // CHECK IF firstSelected IS ALREADY SET
-//   if (firstSelected) return;
+// --=== Deprecated Logic without query ==--
+//
+// const matchingPools = vaultsQuery?.vaults.filter((p) => {
 //   if (formData.leverageTier) {
-//     const newPools = mockPools.filter(
-//       (p) =>
-//         LeverageTiers[p.leverageTier] ===
-//         LeverageTiers[parseInt(formData.leverageTier) as LeverageTier],
-//     );
-//     setfirstSelected(DropsList.leverageTier);
-//     setMatchingPools(newPools);
-//   }
-//   if (formData.versus) {
-//     const newPools = mockPools.filter(
-//       (p) => p.collateralToken === formData.versus,
-//     );
-
-//     setfirstSelected(DropsList.versus);
-//     setMatchingPools(newPools);
-//   }
-//   if (formData.long) {
-//     const newPools = mockPools.filter((p) => {
-//       return p.debtToken === formData.long;
-//     });
-
-//     setfirstSelected(DropsList.long);
-//     setMatchingPools(newPools);
-//   }
-// }, [formData.leverageTier, formData.versus, formData.long]);
-
-// // remove firstSelected if they are now undefined/null
-// useEffect(() => {
-//   if (!firstSelected) return;
-//   if (firstSelected === DropsList.leverageTier) {
-//     if (!formData.leverageTier) {
-//       setfirstSelected(undefined);
-//       setMatchingPools(mockPools);
-//     } else {
-//       const newPools = mockPools.filter(
-//         (p) =>
-//           LeverageTiers[p.leverageTier] ===
-//           LeverageTiers[parseInt(formData.leverageTier) as LeverageTier],
-//       );
-//       setMatchingPools(newPools);
-//       setSecondSelected(undefined);
+//     if (p.leverageTier !== parseInt(formData.leverageTier)) {
+//       return false;
 //     }
 //   }
-//   if (firstSelected === DropsList.versus && !formData.versus) {
-//     setfirstSelected(undefined);
-//     setMatchingPools(mockPools);
+//   if (formData.long) {
+//     if (formData.long.split(",")[0] !== p.collateralToken) {
+//       return false;
+//     }
 //   }
-//   if (firstSelected === DropsList.long && !formData.long) {
-//     setfirstSelected(undefined);
-//     setMatchingPools(mockPools);
+//   if (formData.versus) {
+//     if (formData.versus.split(",")[0] !== p.debtToken) {
+//       return false;
+//     }
 //   }
-// }, [formData.leverageTier, formData.versus, formData.long, firstSelected]);
-
-// // Find selections for all dropdowns
-// const { versus, leverageTiers, long } = useMemo(() => {
-//   // TODO
-//   // also filter for second option selected
-//   // check if select is firstSelected select
-//   let leverageTiers: LeverageTier[] = [];
-//   let versus: string[] = [];
-//   let long: string[] = [];
-
-//   if (firstSelected !== DropsList.leverageTier) {
-//     const leverageArr = matchingPools.map((e) => e.leverageTier);
-//     // Get Unique leverage tiers
-//     leverageTiers = [...new Set(leverageArr)];
-//   } else {
-//     const leverageArr = mockPools.map((e) => e.leverageTier);
-//     // Get Unique leverage tiers
-//     leverageTiers = [...new Set(leverageArr)];
-//   }
-
-//   if (firstSelected !== DropsList.versus) {
-//     versus = matchingPools.map((e) => {
-//       return e.collateralToken;
-//     });
-//   } else {
-//     versus = mockPools.map((e) => e.collateralToken);
-//   }
-
-//   if (firstSelected !== DropsList.long) {
-//     long = matchingPools.map((e) => {
-//       return e.debtToken;
-//     });
-//   } else {
-//     long = mockPools.map((e) => {
-//       return e.debtToken;
-//     });
-//   }
-
-//   return { versus, leverageTiers, long };
-// }, [matchingPools, parent]);
+//   return true;
+// });
