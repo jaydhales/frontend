479d41af179957d85940e1c5b114fb990b757435
Tarp <travistarpley0@gmail.com>
2024-12-18 13:24:41 -0800

    feat: added debouncing param

--- src/components/shared/hooks/useDebounce.ts
+++ src/components/shared/hooks/useDebounce.ts
@@ -2,16 +2,19 @@ import { useEffect, useState } from "react";
 
 export const useDebounce = <T>(value: T, milliSeconds: number) => {
   const [debouncedValue, setDebouncedValue] = useState(value);
-
+  const [debouncing, setDebouncing] = useState(false);
   useEffect(() => {
+    setDebouncing(true);
     const handler = setTimeout(() => {
       setDebouncedValue(value);
+      setDebouncing(false);
     }, milliSeconds);
 
     return () => {
       clearTimeout(handler);
+      setDebouncing(false);
     };
   }, [value, milliSeconds]);
 
-  return debouncedValue;
+  return { debouncedValue, debouncing };
 };
