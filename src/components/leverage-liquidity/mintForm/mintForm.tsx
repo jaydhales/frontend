a5f380a3ca88d9ffc2d8b36db1e7b5e98a006fed
Tarp <travistarpley0@gmail.com>
2025-01-06 12:09:32 -0800

    feat: Added clear btn to clear Vault Param Selections

--- src/components/leverage-liquidity/mintForm/mintForm.tsx
+++ src/components/leverage-liquidity/mintForm/mintForm.tsx
@@ -32,7 +32,6 @@ import { useResetTransactionModal } from "./hooks/useResetTransactionModal";
 import ErrorMessage from "@/components/ui/error-message";
 import { useCalculateMaxApe } from "./hooks/useCalculateMaxApe";
 import { useFilterVaults } from "./hooks/useFilterVaults";
-import useVaultFilterStore from "@/lib/store";
 interface Props {
   vaultsQuery: TVaults;
   isApe: boolean;
