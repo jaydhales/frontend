fe8cbdba9dc06dc1b58b1b8aaf46decaae4d808a
Tarp <travistarpley0@gmail.com>
2025-01-22 10:54:54 -0800

    fix: fixed quoterv2 abi

--- src/contracts/uniswap-quoterv2.ts
+++ src/contracts/uniswap-quoterv2.ts
@@ -5,44 +5,80 @@ export const UniswapQuoterV2 = {
   abi: [
     {
       inputs: [
-        { internalType: "address", name: "factory", type: "address" },
-        { internalType: "address", name: "WETH9", type: "address" },
+        { internalType: "address", name: "_factory", type: "address" },
+        { internalType: "address", name: "_WETH9", type: "address" },
       ],
       stateMutability: "nonpayable",
       type: "constructor",
     },
     {
+      inputs: [],
+      name: "WETH9",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      inputs: [],
+      name: "factory",
+      outputs: [{ internalType: "address", name: "", type: "address" }],
+      stateMutability: "view",
+      type: "function",
+    },
+    {
       inputs: [
+        { internalType: "bytes", name: "path", type: "bytes" },
         { internalType: "uint256", name: "amountIn", type: "uint256" },
-        { internalType: "address[]", name: "path", type: "address[]" },
       ],
       name: "quoteExactInput",
       outputs: [
         { internalType: "uint256", name: "amountOut", type: "uint256" },
+        {
+          internalType: "uint160[]",
+          name: "sqrtPriceX96AfterList",
+          type: "uint160[]",
+        },
+        {
+          internalType: "uint32[]",
+          name: "initializedTicksCrossedList",
+          type: "uint32[]",
+        },
+        { internalType: "uint256", name: "gasEstimate", type: "uint256" },
       ],
-      stateMutability: "view",
+      stateMutability: "nonpayable",
       type: "function",
     },
     {
       inputs: [
-        { internalType: "uint256", name: "amountOut", type: "uint256" },
-        { internalType: "address[]", name: "path", type: "address[]" },
-      ],
-      name: "quoteExactOutput",
-      outputs: [{ internalType: "uint256", name: "amountIn", type: "uint256" }],
-      stateMutability: "view",
-      type: "function",
-    },
-    {
-      inputs: [
-        { internalType: "bytes", name: "path", type: "bytes" },
-        { internalType: "uint256", name: "amountIn", type: "uint256" },
+        {
+          components: [
+            { internalType: "address", name: "tokenIn", type: "address" },
+            { internalType: "address", name: "tokenOut", type: "address" },
+            { internalType: "uint256", name: "amountIn", type: "uint256" },
+            { internalType: "uint24", name: "fee", type: "uint24" },
+            {
+              internalType: "uint160",
+              name: "sqrtPriceLimitX96",
+              type: "uint160",
+            },
+          ],
+          internalType: "struct IQuoterV2.QuoteExactInputSingleParams",
+          name: "params",
+          type: "tuple",
+        },
       ],
       name: "quoteExactInputSingle",
       outputs: [
         { internalType: "uint256", name: "amountOut", type: "uint256" },
+        { internalType: "uint160", name: "sqrtPriceX96After", type: "uint160" },
+        {
+          internalType: "uint32",
+          name: "initializedTicksCrossed",
+          type: "uint32",
+        },
+        { internalType: "uint256", name: "gasEstimate", type: "uint256" },
       ],
-      stateMutability: "view",
+      stateMutability: "nonpayable",
       type: "function",
     },
     {
@@ -50,8 +86,65 @@ export const UniswapQuoterV2 = {
         { internalType: "bytes", name: "path", type: "bytes" },
         { internalType: "uint256", name: "amountOut", type: "uint256" },
       ],
+      name: "quoteExactOutput",
+      outputs: [
+        { internalType: "uint256", name: "amountIn", type: "uint256" },
+        {
+          internalType: "uint160[]",
+          name: "sqrtPriceX96AfterList",
+          type: "uint160[]",
+        },
+        {
+          internalType: "uint32[]",
+          name: "initializedTicksCrossedList",
+          type: "uint32[]",
+        },
+        { internalType: "uint256", name: "gasEstimate", type: "uint256" },
+      ],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        {
+          components: [
+            { internalType: "address", name: "tokenIn", type: "address" },
+            { internalType: "address", name: "tokenOut", type: "address" },
+            { internalType: "uint256", name: "amount", type: "uint256" },
+            { internalType: "uint24", name: "fee", type: "uint24" },
+            {
+              internalType: "uint160",
+              name: "sqrtPriceLimitX96",
+              type: "uint160",
+            },
+          ],
+          internalType: "struct IQuoterV2.QuoteExactOutputSingleParams",
+          name: "params",
+          type: "tuple",
+        },
+      ],
       name: "quoteExactOutputSingle",
-      outputs: [{ internalType: "uint256", name: "amountIn", type: "uint256" }],
+      outputs: [
+        { internalType: "uint256", name: "amountIn", type: "uint256" },
+        { internalType: "uint160", name: "sqrtPriceX96After", type: "uint160" },
+        {
+          internalType: "uint32",
+          name: "initializedTicksCrossed",
+          type: "uint32",
+        },
+        { internalType: "uint256", name: "gasEstimate", type: "uint256" },
+      ],
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      inputs: [
+        { internalType: "int256", name: "amount0Delta", type: "int256" },
+        { internalType: "int256", name: "amount1Delta", type: "int256" },
+        { internalType: "bytes", name: "path", type: "bytes" },
+      ],
+      name: "uniswapV3SwapCallback",
+      outputs: [],
       stateMutability: "view",
       type: "function",
     },
