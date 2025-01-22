a6233d91f81a041e7676985329f5dd8b927df71f
Tarp <travistarpley0@gmail.com>
2025-01-22 14:31:05 -0800

    feat: Added uniswapQuote query and minDeposit arg

--- src/components/shared/hooks/useMintApeOrTea.ts
+++ src/components/shared/hooks/useMintApeOrTea.ts
@@ -3,15 +3,19 @@ import { useSimulateContract } from "wagmi";
 import type { TAddressString } from "@/lib/types";
 import { VaultContract } from "@/contracts/vault";
 import { useEffect } from "react";
+import { api } from "@/trpc/react";
+import { formatUnits } from "viem";
 interface Props {
   collateralToken: string;
   debtToken: string;
   amount: bigint | undefined;
   tokenAllowance: bigint | undefined;
+  depositToken: string;
   leverageTier: number;
   vaultId: string | undefined;
   isApe: boolean;
   useEth: boolean;
+  decimals: number;
 }
 export function useMintApeOrTea({
   collateralToken,
@@ -21,12 +25,24 @@ export function useMintApeOrTea({
   tokenAllowance,
   isApe,
   useEth,
+  depositToken,
+  decimals,
 }: Props) {
   const vault = {
     debtToken: debtToken as TAddressString,
     collateralToken: collateralToken as TAddressString,
     leverageTier,
   };
+  const debtTokenDeposit = depositToken === debtToken && debtToken !== "";
+  const { data: uniswapQuote } = api.quote.getUniswapSwapQuote.useQuery(
+    {
+      amount: formatUnits(amount ?? 0n, 18),
+      decimals,
+      tokenAddressA: collateralToken,
+      tokenAddressB: debtToken,
+    },
+    { enabled: debtTokenDeposit },
+  );
   const tokenAmount = useEth ? 0n : amount;
   const ethAmount = useEth ? amount : 0n;
   const {
@@ -42,12 +58,13 @@ export function useMintApeOrTea({
       { ...vault },
       // isApe ? apeAddress : zeroAddress,
       tokenAmount ?? 0n,
+      debtTokenDeposit ? uniswapQuote?.amountOut ?? 0n : 0n,
     ],
     value: ethAmount ?? 0n,
   });
 
   if (error) {
-    console.log(error, "APE OR TEA ERROR");
+    // console.log(error, "APE OR TEA ERROR");
   }
   useEffect(() => {
     refetch().catch((e) => console.log(e));
