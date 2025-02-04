17b555ef1e93cd6c67d667d1e15c28b34927cfc4
Tarp <travistarpley0@gmail.com>
2025-02-04 15:51:22 -0800

    feat: moved fetch user bal to another hook and added docs

--- src/components/leverage-liquidity/mintForm/hooks/useGetUserBals.ts
+++ src/components/leverage-liquidity/mintForm/hooks/useGetUserBals.ts
@@ -1,13 +1,33 @@
 import type { TMintFormFields } from "@/components/providers/mintFormProvider";
+import { VaultContract } from "@/contracts/vault";
 import { parseAddress } from "@/lib/utils";
 import { api } from "@/trpc/react";
 import { useFormContext } from "react-hook-form";
 import { useAccount } from "wagmi";
-
-export default function useGetUserBals() {
+/**
+ * Gets user balance and allowance for the form field deposit token
+ * Gets user ETH Balance
+ * Gets Collateral and Debt Decimals
+ */
+export default function useGetFormTokensInfo() {
   const { address } = useAccount();
-  const form = useFormContext<TMintFormFields>();
-  const formData = form.watch();
+
+  const formData = useFormContext<TMintFormFields>().watch();
+
+  const { data: userBalance, isFetching } =
+    api.user.getBalanceAndAllowance.useQuery(
+      {
+        userAddress: address,
+        tokenAddress: formData.depositToken,
+        spender: VaultContract.address,
+      },
+      {
+        enabled:
+          Boolean(address) &&
+          Boolean(formData.long) &&
+          Boolean(formData.depositToken),
+      },
+    );
   const { data: userEthBalance } = api.user.getEthBalance.useQuery(
     { userAddress: address },
     { enabled: Boolean(address) && Boolean(formData.long) },
@@ -33,5 +53,12 @@ export default function useGetUserBals() {
     formData.depositToken === parseAddress(formData.long)
       ? collateralDecimals
       : debtDecimals;
-  return { userEthBalance, collateralDecimals, debtDecimals, depositDecimals };
+  return {
+    userEthBalance,
+    userBalance,
+    userBalanceFetching: isFetching,
+    collateralDecimals,
+    debtDecimals,
+    depositDecimals,
+  };
 }
