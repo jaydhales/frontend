822037872b5ab5ff0da8733f00c73aa694b07967
Codonyat <nil.garcia@gmail.com>
2025-02-20 22:24:43 +0100

    testing whitespace pre wrap

--- src/components/shared/explainer.tsx
+++ src/components/shared/explainer.tsx
@@ -91,7 +91,7 @@ export default function Explainer({ page }: Props) {
       >
         <CloseModalButton close={close} />
         <h1 className="text-[20px] font-bold">{Explainers[page].title}</h1>
-        <p className="pt-2 text-[16px] font-medium leading-5">
+        <p className="pt-2 text-[16px] font-medium leading-5 whitespace-pre-wrap">
           {Explainers[page].description}
         </p>
       </Card>
