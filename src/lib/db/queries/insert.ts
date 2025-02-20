139330656e75fcefce5e93cce12b3bf90bdb4d07
Tarp <travistarpley0@gmail.com>
2025-02-19 18:01:25 -0800

    feat: added aggregate table and insert

--- src/lib/db/queries/insert.ts
+++ src/lib/db/queries/insert.ts
@@ -1,12 +1,19 @@
 import { db } from "../db";
-import type { InsertAprs } from "../schema";
-import { aprsTable } from "../schema";
+import type { InsertAprs, InsertLastMonthApr } from "../schema";
+import { aprsTable, lastMonthApr } from "../schema";
 
-export async function createApr(data: InsertAprs) {
+export async function insertApr(data: InsertAprs) {
   try {
     const query = await db.insert(aprsTable).values(data);
     return query;
   } catch (error) {
     console.error("Error inserting data:", error);
   }
+}
+export async function insertOrUpdateAprAggregate(data: InsertLastMonthApr) {
+  const query = await db
+    .insert(lastMonthApr)
+    .values(data)
+    .onConflictDoUpdate({ target: lastMonthApr.id, set: data });
+  return query;
 }
--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,4 +1,4 @@
-import { text, integer, pgTable, uuid } from "drizzle-orm/pg-core";
+import { text, integer, pgTable, uuid, unique } from "drizzle-orm/pg-core";
 
 export const aprsTable = pgTable("apr_rates", {
   id: uuid("id").defaultRandom().primaryKey(),
@@ -6,5 +6,18 @@ export const aprsTable = pgTable("apr_rates", {
   timestamp: integer("timestamp").notNull(),
 });
 
+export const lastMonthApr = pgTable(
+  "last_month_apr",
+  {
+    id: integer("id").primaryKey().default(1), // Always ID 1
+    apr: text("apr").notNull(),
+    latestTimestamp: integer("timestamp").notNull(),
+  },
+  (table) => [
+    unique().on(table.id), // Ensures only one row
+  ],
+);
 export type InsertAprs = typeof aprsTable.$inferInsert;
 export type SelectAprs = typeof aprsTable.$inferSelect;
+export type InsertLastMonthApr = typeof lastMonthApr.$inferInsert;
+export type SelectLastMonthApr = typeof lastMonthApr.$inferSelect;
