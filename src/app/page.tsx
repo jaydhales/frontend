3fa0bd75be0ff07c22698e0d6595a74131059a43
Tarp <travistarpley0@gmail.com>
2025-02-26 14:32:24 -0800

    fix: remove params

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,14 @@
 import LeverageLiquidityContent from "@/components/leverage-liquidity/leverageLiquidityContent";
 import LeverageLiquidityPage from "@/components/leverage-liquidity/leverageLiquidityPage";
 import MintForm from "@/components/leverage-liquidity/mintForm/mintForm";
-import { getVaultsForTable } from "@/lib/getVaults";
 export const revalidate = 0;
 export default async function Home() {
-  const { vaultQuery } = await getVaultsForTable(0);
   return (
     <main className="flex  flex-col items-center justify-center text-white">
       <LeverageLiquidityPage title={"Take on Leverage"}>
         <LeverageLiquidityContent
           isApe
-          form={<MintForm isApe vaultsQuery={vaultQuery} />}
-          vaultsQuery={vaultQuery}
+          form={<MintForm isApe />}
         ></LeverageLiquidityContent>
       </LeverageLiquidityPage>
     </main>
