75faa7d276d2487f8fe47028256b6866329d1384
Tarp <travistarpley0@gmail.com>
2025-02-26 14:31:18 -0800

    fix: type

--- src/app/liquidity/[slug]/page.tsx
+++ src/app/liquidity/[slug]/page.tsx
@@ -1,22 +1,16 @@
 import LeverageLiquidityContent from "@/components/leverage-liquidity/leverageLiquidityContent";
 import LeverageLiquidityPage from "@/components/leverage-liquidity/leverageLiquidityPage";
 import MintForm from "@/components/leverage-liquidity/mintForm/mintForm";
-import { getVaultsForTable } from "@/lib/getVaults";
 
 // import { unstable_cache } from "next/cache";
 export const revalidate = 10;
-export default async function Home({ params }: { params: { slug: string } }) {
-  const { slug } = params;
-  const offset = isFinite(parseInt(slug)) ? parseInt(slug) : 0;
-  const { vaultQuery } = await getVaultsForTable(offset);
-
+export default async function Home() {
   return (
     <main className="flex  flex-col items-center justify-center text-white">
       <LeverageLiquidityPage title={"Take on Leverage"}>
         <LeverageLiquidityContent
           isApe
-          form={<MintForm isApe vaultsQuery={vaultQuery} />}
-          vaultsQuery={vaultQuery}
+          form={<MintForm isApe />}
         ></LeverageLiquidityContent>
       </LeverageLiquidityPage>
     </main>
