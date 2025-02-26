2bd95c7132dd21d2fd1b6cebd9063e2e3f384cfe
Tarp <travistarpley0@gmail.com>
2025-02-26 14:31:55 -0800

    fix: remove unused param

--- src/app/liquidity/page.tsx
+++ src/app/liquidity/page.tsx
@@ -1,18 +1,15 @@
 import LeverageLiquidityContent from "@/components/leverage-liquidity/leverageLiquidityContent";
 import LeverageLiquidityPage from "@/components/leverage-liquidity/leverageLiquidityPage";
 import MintForm from "@/components/leverage-liquidity/mintForm/mintForm";
-import { getVaultsForTable } from "@/lib/getVaults";
 
 export const revalidate = 0;
 export default async function Home() {
-  const { vaultQuery } = await getVaultsForTable(0);
   return (
     <main className="flex  flex-col items-center justify-center text-white">
       <LeverageLiquidityPage title="Provide Liquidity">
         <LeverageLiquidityContent
           isApe={false}
-          form={<MintForm isApe vaultsQuery={vaultQuery} />}
-          vaultsQuery={vaultQuery}
+          form={<MintForm isApe />}
         ></LeverageLiquidityContent>
       </LeverageLiquidityPage>
     </main>
