921bf56c7c1b3e09eb922464e858a663389ccfef
Codonyat <nil.garcia@gmail.com>
2025-02-27 14:41:42 +0100

    feat: hover message over APR

--- src/components/stake/stakeData/aprCard.tsx
+++ src/components/stake/stakeData/aprCard.tsx
@@ -4,7 +4,14 @@ import React from "react";
 import AprDisplay from "./aprDisplay";
 import { syncDividends } from "@/lib/dividendsSync";
 import { selectPayouts } from "@/lib/db/queries/select";
+import {
+  HoverCard,
+  HoverCardTrigger,
+  HoverCardContent,
+} from "@/components/ui/hover-card";
+
 export const dynamic = "force-dynamic";
+
 export default async function AprCard() {
   let apr = await api.divends.getApr();
   const dividendsPaidRequest = await executeGetDividendGreaterThan({
@@ -23,7 +30,23 @@ export default async function AprCard() {
   return (
     <div className="flex flex-col items-center justify-center gap-2 rounded-md bg-secondary py-2">
       <div className="flex w-full flex-row items-center justify-center">
-        <div className="px-2 text-sm text-gray-300">Staking APR</div>
+        {/* Wrapped "Staking APR" with HoverCard */}
+        <HoverCard>
+          <HoverCardTrigger asChild>
+            <div className="px-2 text-sm text-gray-300 cursor-pointer">
+              Staking APR
+            </div>
+          </HoverCardTrigger>
+          <HoverCardContent side="top" alignOffset={10}>
+            <div className="mb-2 max-w-[200px] rounded-sm bg-white px-2 py-2 text-[13px] font-medium text-gray-800">
+              <span>
+                The APR is estimated from last month's dividend data.
+                While SIR does not trade in secondary markets, we use the presale price of $0.00016542597.
+              </span>
+            </div>
+          </HoverCardContent>
+        </HoverCard>
+        {/* <div className="px-2 text-sm text-gray-300">Staking APR</div> */}
       </div>
       <div className="font-normal text-2xl ">
         <AprDisplay currentApr={apr} />
