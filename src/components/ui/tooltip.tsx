07236a2c32c412e3cf89c8af7663b4b218f2aa5b
Tarp <travistarpley0@gmail.com>
2025-03-03 11:50:35 -0800

    feat: added class variants to tooltip

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -7,21 +7,31 @@ import {
   HoverCardContent,
   HoverCardTrigger,
 } from "@radix-ui/react-hover-card";
-interface TooltipsProps {
+import type { VariantProps } from "class-variance-authority";
+import { cva } from "class-variance-authority";
+
+const tooltipVariants = cva(
+  "max-w-[200px] rounded-md bg-white px-2 py-2 text-gray-800 text-center",
+  {
+    variants: {
+      size: { "200": "max-w-[200px]", "300": "max-w-[300px]" },
+    },
+    defaultVariants: { size: "200" },
+  },
+);
+interface TooltipsProps extends VariantProps<typeof tooltipVariants> {
   children: React.ReactNode;
-  size?: number;
+  iconSize?: number;
 }
 
-const ToolTip: FC<TooltipsProps> = ({ children, size }) => {
+const ToolTip: FC<TooltipsProps> = ({ children, iconSize, size }) => {
   return (
     <HoverCard openDelay={0} closeDelay={20}>
       <HoverCardTrigger>
-        <Info size={size ?? 16} />
+        <Info size={iconSize ?? 16} />
       </HoverCardTrigger>
       <HoverCardContent side="top" alignOffset={10}>
-        <div className="max-w-[200px] rounded-md bg-white px-2 py-2 text-gray-800 text-center">
-          {children}
-        </div>
+        <div className={tooltipVariants({ size })}>{children}</div>
         <HoverCardArrow className="fill-white" height={15} width={14} />
       </HoverCardContent>
     </HoverCard>
