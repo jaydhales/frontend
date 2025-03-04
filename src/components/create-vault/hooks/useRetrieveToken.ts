7f1a4a7e76386c7613716573e03184b6b1dc26ba
Tarp <travistarpley0@gmail.com>
2025-03-04 11:30:36 -0800

    feat: retrieve token
    used to get name and symbol from token address, and check and return
    checksum address

---
+++ src/components/create-vault/hooks/useRetrieveToken.ts
@@ -0,0 +1,38 @@
+import { useMemo } from "react";
+import { erc20Abi, getAddress, zeroAddress } from "viem";
+import { useReadContracts } from "wagmi";
+
+export default function useRetrieveToken({
+  tokenAddress,
+}: {
+  tokenAddress: string;
+}) {
+  const { address } = useMemo(() => {
+    try {
+      const checkedAddress = getAddress(tokenAddress);
+      return { address: checkedAddress };
+    } catch {
+      return { address: null };
+    }
+  }, [tokenAddress]);
+  const { data } = useReadContracts({
+    contracts: [
+      {
+        address: address ?? zeroAddress,
+        abi: erc20Abi,
+        functionName: "symbol",
+      },
+      {
+        address: address ?? zeroAddress,
+        abi: erc20Abi,
+        functionName: "name",
+      },
+    ],
+    query: {
+      enabled: !!address,
+    },
+  });
+  const symbol = data?.[0].result;
+  const name = data?.[1].result;
+  return { symbol, name, address };
+}
