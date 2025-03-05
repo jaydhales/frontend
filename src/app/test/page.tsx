901b7ca24942ca64f157baf5454758eccac5787d
Tarp <travistarpley0@gmail.com>
2025-03-04 16:26:09 -0800

    fix: don't minus the amount of zeros

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -5,35 +5,35 @@ import React, { useMemo } from "react";
 export default function Page() {
   return (
     <div className="flex justify-center">
-      <div className="rounded-md bg-black p-4">
-        <DisplaySmallNumber num={0.00031} />
-        <DisplaySmallNumber num={0.0000312222} />
-        <DisplaySmallNumber num={0.000000512222} />
+      <div className="flex flex-col gap-4 rounded-md bg-black p-4 text-white">
+        <span className="text-md text-white">
+          <DisplaySmallNumber num={formatSmallNumber(0.00031)} />
+        </span>
+        <span>
+          <DisplaySmallNumber num={formatSmallNumber(0.0000312222)} />
+        </span>
       </div>
     </div>
   );
 }
 
-function DisplaySmallNumber({ num }: { num: number }) {
-  const dis = formatSmallNumber(num);
-  console.log(num);
+function DisplaySmallNumber({ num }: { num: string }) {
   const { sig, exp } = useMemo(() => {
-    if (dis.includes("v")) {
-      const split = dis.split("v")[1];
+    if (num.includes("v")) {
+      const split = num.split("v")[1];
       if (!split) return { sig: "0", exp: 0 };
       return { sig: split.slice(1, split.length), exp: split[0] };
     }
     return { sig: "0", exp: 0 };
-  }, [dis]);
-
+  }, [num]);
+  if (!num.includes("v")) {
+    return num;
+  }
   return (
-    <div className="flex gap-x-4 text-white">
-      <div>{num}</div>
-      <div>
-        0.0
-        <sub className="text-[11px]">+{exp}</sub>
-        {sig}
-      </div>
-    </div>
+    <>
+      0.0
+      <sub className="text-[11px]">+{exp}</sub>
+      {sig}
+    </>
   );
 }
