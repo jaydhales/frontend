280757a7ba78cc0dcef96dccaae8e1dcf2681966
abstrucked <mxhgraphics@gmail.com>
2025-03-09 19:56:26 +0100

    abstrucked/eng-353-bottom-menu-with-basic-links
    footer content added
    !!missing icons

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,9 +2,19 @@
 
 export default function Footer() {
   return (
-    <div className="flex  flex-grow items-end px-[16px] py-[32px]">
-      {/* <div className="flex w-full items-end justify-between"> */}
-      {/* </div> */}
+    <div className="flex flex-grow items-end px-[16px] py-[32px]">
+      <div className="flex w-full items-end justify-between">
+        <nav className="flex gap-x-4 text-[14px] text-muted-foreground ">
+          <a className="hover:text-white" href="https://docs.sir.trading">Docs</a>
+          <a className="hover:text-white" href="https://www.sir.trading/audit">Audit</a>
+          <a className="hover:text-white" href="https://docs.sir.trading/protocol-overview/contract-addresses">Contract addresses</a>
+        </nav>
+        <nav className="flex gap-x-4 text-[14px] text-muted-foreground ">
+          <a className="hover:text-white" href="https://x.com/leveragesir">X</a>
+          <a className="hover:text-white" href="https://discord.gg/M2SRBDPUR2">Discord</a>
+          <a className="hover:text-white" href="https://github.com/SIR-trading">GitHub</a>
+        </nav>
+      </div>
     </div>
   );
 }
