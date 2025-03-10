d07b7150e49aea74f3d7b79a936f2919a7b96908
Tarp <travistarpley0@gmail.com>
2025-03-10 11:13:32 -0700

    feat: share vault table accross lev/liq/calculator

--- src/components/leverage-calculator/leverageCalculatorContent.tsx
+++ src/components/leverage-calculator/leverageCalculatorContent.tsx
@@ -2,11 +2,9 @@ import MintFormProvider from "@/components/providers/mintFormProvider";
 import Pagination from "@/components/shared/pagination";
 import { Card } from "@/components/ui/card";
 import type { TVaults } from "@/lib/types";
-import { EPage } from "@/lib/types";
 import { Container } from "@/components/ui/container";
-import VaultTable from "./vaultTable/vaultTable";
 import CalculatorForm from "./calculatorForm/calculatorForm";
-import Explainer from "../shared/explainer";
+import VaultTable from "../leverage-liquidity/vaultTable/vaultTable";
 
 export default function LeverageCalculatorContent({
   vaultsQuery,
@@ -24,7 +22,7 @@ export default function LeverageCalculatorContent({
           <CalculatorForm vaultsQuery={vaultsQuery} isApe={isApe} />
           <Card>
             <div className="flex h-full flex-col justify-between">
-              <VaultTable isApe={isApe} vaultQuery={vaultsQuery} />
+              <VaultTable isApe={isApe} />
               <Pagination
                 max={Math.ceil((vaultsQuery?.vaults.length ?? 0) / 8)}
               />
