2871e078d651d03c8ac488b14a48ac92a91cb7a0
Tarp <travistarpley0@gmail.com>
2025-03-18 16:26:29 -0700

    fix: mobile sizing

--- src/components/ui/error-message.tsx
+++ src/components/ui/error-message.tsx
@@ -6,7 +6,7 @@ export default function ErrorMessage({
   disabled?: boolean;
 }) {
   return (
-    <div className="flex w-[450px]  justify-start pt-[2px]">
+    <div className="flex w-[280px] justify-start  pt-[2px] md:w-[450px]">
       <p
         data-disabled={disabled ? "true" : "false"}
         className="h-[13px] text-left text-sm text-red-400 data-[disabled=true]:opacity-0"
